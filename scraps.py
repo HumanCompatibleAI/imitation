4cc099279b8c9ac4fc47504d7908130dd3d7fae1
Steven H. Wang <wang.steven.h@gmail.com>
2018-12-11 15:38:37 -0800

    Pickled CartPole policy
    We have an easier time with CartPole for some reason. Maybe
    because the reward is less sparse than before?

--- scraps.py
+++ scraps.py
@@ -2,14 +2,16 @@
 MVP-not-really
 """
 
+import numpy as np
 import gym
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO1
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines import PPO1, PPO2
 
-def _frozen_lake():
+
+def _basics1(env_name="CartPole-v1", skip_training=True):
     """
-    * Load frozen lake environment.
+    * Load X environment.
     * Train a PPO policy on frozen lake.
     * Generate 9 trajectory rollouts.
     * Print the state action pairs.
@@ -18,18 +20,56 @@ def _frozen_lake():
     # Load the policy.
     # Repeat the procedure above from the rollout part.
     """
-    env = gym.make("FrozenLake-v0")
-    env = DummyVecEnv([lambda: env])
+    orig_env = gym.make(env_name)
+    # # PPO1
+    env = DummyVecEnv([lambda: orig_env])
+    savepath = "saved_models/ppo_"+env_name
+    if not skip_training:
+        model = PPO1(MlpPolicy, env, verbose=1,
+                tensorboard_log="./output/{}/".format(env_name))
+        model.learn(50000)
+        model.save(savepath)
+    else:
+        model = PPO1.load(savepath, env=env)
 
-    model = PPO1(MlpPolicy, env, verbose=1)
-    model.learn(50000)
+    X = generate_rollouts(model, orig_env, n_timesteps=3)
+    print(X)
 
-    for i in range(2):
+
+def generate_rollouts(model, env, n_timesteps):
+    """
+    Generate state-action pairs from a policy.
+
+    Params:
+    model -- A stable_baselines Model, trained on the gym environment.
+    env (Env) -- A Gym Env. VecEnv is not currently supported.
+    n_timesteps (int) -- The number of state-action pairs to collect.
+
+    Return:
+    rollout_obs (array) -- A numpy array with shape
+      `[n_timesteps] + env.observation_space.shape`.
+    rollout_act (array) -- A numpy array with shape
+      `[n_timesteps] + env.action_space.shape`.
+    """
+    rollout_obs = []
+    rollout_act = []
+    while len(rollout_obs) < n_timesteps:
         done = False
         obs = env.reset()
-        while not done:
-            a, _ = model.predict(obs)
-            obs, reward, done, info = env.step(a)
-            env.render()
+        while not done and len(rollout_obs) < n_timesteps:
+            act, _ = model.predict(obs)
+            rollout_obs.append(obs)
+            rollout_act.append(act)
+            obs, _, done, _ = env.step(act)
 
-_frozen_lake()
+    rollout_obs = np.array(rollout_obs)
+    exp_obs = (n_timesteps,) + env.observation_space.shape
+    assert rollout_obs.shape == exp_obs
+
+    rollout_act = np.array(rollout_act)
+    exp_act = (n_timesteps,) + env.action_space.shape
+    assert rollout_act.shape == exp_act
+
+    return rollout_obs, rollout_act
+
+_basics1()
