f0ffdb7ff3f11311baa5902899fd5ef7fe457291
Steven H. Wang <wang.steven.h@gmail.com>
2018-12-11 22:35:29 -0800

    Make RewardNet into an abstract class

--- reward_net.py
+++ reward_net.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
-import tf.keras as keras
-from keras import models, layers
-
-# Immediate TODO: Use tf.layers instead of keras.
+from abc import ABC, abstractmethod
 
 
-class RewardNet():
+class RewardNet(ABC):
 
     def __init__(self, env, state_only=True, discount_factor=0.9):
         """
@@ -26,43 +23,90 @@ class RewardNet():
         self.discount_factor = discount_factor
 
         phs = self._build_placeholders()
-        self.old_obs_ph, self.action_ph, self.new_obs_ph = phs
+        self.old_obs_ph, self.act_ph, self.new_obs_ph = phs
 
-        old_obs_input = keras.Input(tensor=obs_ph, name="old_obs_input")
-        action_input = keras.Input(tensor=action_ph, name="action_input")
-        new_obs_input = keras.Input(tensor=new_obs_ph, name="new_obs_input")
+        self.reward_output = self.build_theta_network(
+                self.old_obs_ph, self.act_ph)
 
-        self.theta_network, reward = self._build_theta_network(
-                old_obs_input, action_input)
-        self.phi_network, old_shaping, new_shaping = self._build_phi_network(
-                old_obs_input, new_obs_input)
-        self.f_network = self._build_f_network(old_obs_input, action_input,
-                new_obs_input, reward, old_shaping, new_shaping)
+        old_shaping_output, new_shaping_output = self.build_phi_network(
+                self.old_obs_ph, self.new_obs_ph)
+
+        self.shaped_reward_output = (self.reward_output +
+                self.discount_factor * new_shaping_output - old_shaping_output)
+
 
     def _build_placeholders(self):
         """
         Returns old_obs, action, new_obs
         """
-        obs_size = TODO(self.env.obs_space)
-        action_size = TODO(self.env.action_space)
+        o_shape = (None,) + self.env.observation_space.shape
+        a_shape = (None,) + self.env.action_space.shape
 
-        # TODO: What about discrete spaces? Should I configure one_hot
-        # inputs instead?
-        old_obs_ph = tf.placeholder(dtype=tf.float32, shape=[None, obs_size])
-        action_ph = tf.placeholder(dtype=tf.float32, shape=[None, action_size])
-        new_obs_ph = tf.placeholder(dtype=tf.float32, shape=[None, obs_size])
-        return old_obs, action_ph, new_obs_ph
+        old_obs_ph = tf.placeholder(name="old_obs_ph",
+                dtype=tf.float32, shape=o_shape)
+        new_obs_ph = tf.placeholder(name="new_obs_ph",
+                dtype=tf.float32, shape=o_shape)
+        act_ph = tf.placeholder(name="act_ph",
+                dtype=tf.float32, shape=a_shape)
+
+        return old_obs, act_ph, new_obs_ph
 
 
-    def _build_theta_network(self, obs_input, action_input):
+    @abstractmethod
+    def build_theta_network(self, obs_input, act_input):
         """
         Returns theta_network, a keras Model that predicts reward whose
         input is a state-action pair.
 
         Although AIRL doesn't individually optimize this subnetwork during
         training, it ends up being our reward approximator at test time.
+
+        Params:
+        obs_input (Tensor) -- The observation input. Its shape is
+          `((None,) + self.env.observation_space.shape)`.
+        act_input (Tensor) -- The action input. Its shape is
+          `((None,) + self.env.action_space.shape)`. The None dimension is
+          expected to be the same as None dimension from `obs_input`.
+
+        Return:
+        reward_output (Tensor) -- A reward prediction for each of the
+          inputs. The shape is `(None,)`.
         """
-        inputs = [obs_inputs, action_input]
+        pass
+
+
+    @abstractmethod
+    def build_phi_network(self, old_obs_input, new_obs_input):
+        """
+        Build the reward shaping network (serves to disentangle dynamics from
+        reward).
+
+        XXX: We could potentially make it easier on the subclasser by requiring
+          only one input. ie build_phi_network(obs_input). Later in
+          _build_f_network, I could stack Tensors of old and new observations,
+          pass them simulatenously through the network, and then unstack the
+          outputs.
+
+        Params:
+        old_obs_input (Tensor): The old observations (corresponding to the
+          state at which the current action is made). The shape of this
+          Tensor should be `((None,) + self.env.observation_space.shape)`.
+        new_obs_input (Tensor): The new observations (corresponding to the
+          state that we transition to after this state-action pair.
+
+        Return:
+        shaping_output_old (Tensor) -- A reward shaping prediction for each of
+          the old inputs.
+        shaping_output_new (Tensor) -- A reward shaping prediction for each of
+          the new outputs.
+        """
+        pass
+
+
+class BasicRewardNet(RewardNet):
+
+    def build_theta_network(self, obs_input, act_input):
+        inputs = [obs_inputs, act_input]
 
         if not self.state_only:
             x = layers.concatenate(inputs)
@@ -79,17 +123,13 @@ class RewardNet():
         reward = layers.Dense(1)(x)
 
         model = tf.keras.Model(name="theta",
-                inputs=[obs_input, action_inputs],
+                inputs=[obs_input, act_inputs],
                 outputs=reward)
 
         return model, reward
 
 
-    def _build_phi_network(self, old_obs_input, new_obs_input):
-        """
-        Return phi_network, the reward shaping network (serves to disentangle
-        dynamics from reward).
-        """
+    def build_phi_network(self, old_obs_input, new_obs_input):
         # TODO: Parameter instead of magic # for the '64' part. (This seems less
         # urgent than getting something up and running.)
         shared_layers = [
@@ -107,20 +147,3 @@ class RewardNet():
         model = tf.keras.Model(name="phi", inputs=inputs, outputs=outputs)
 
         return model, old_shaping, new_shaping
-
-
-    def _build_f_network(self, old_obs_input, action_input,
-            new_obs_input, reward, old_shaping, new_shaping):
-        """
-        Combine the theta and phi networks to generate the reward approximator
-        used by the AIRL discriminator. (This network is called f(s, a, s') in
-        the AIRL paper).
-        """
-
-        shaped_reward = reward + self.discount_factor * new_shaping \
-                - old_shaping
-        model = tf.keras.Model(name="f",
-                inputs=[old_obs_input, action_input, new_obs_input],
-                outputs=shaped_reward,
-                )
-        return model
