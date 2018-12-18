d3d258ad9538bb3f0033733cfa89276c3902b973
Steven H. Wang <wang.steven.h@gmail.com>
2018-12-17 16:19:21 -0800

    Document pytest settings

--- conftest.py
+++ conftest.py
@@ -1,9 +1,15 @@
 import pytest
 
+"""
+Configures pytest to ignore unit tests marked as expensive unless we
+use the --expensive flag. The goal of the marking tests as expensive
+is to ensure that the default pytest run should take less than 30
+seconds.
+"""
 
 def pytest_addoption(parser):
-        parser.addoption("--expensive", action="store_true",
-        help="run expensive tests (which are otherwise skipped).")
+    parser.addoption("--expensive", action="store_true",
+            help="run expensive tests (which are otherwise skipped).")
 
 def pytest_runtest_setup(item):
     if 'expensive' in item.keywords and not item.config.getoption(
