ff7114aaa6ad03a8f5961d384fc362157d6a0b97
Steven H. Wang <wang.steven.h@gmail.com>
2018-12-22 22:53:47 -0800

    Make scripts CLI-runnable again

--- scripts.py
+++ scripts.py
@@ -104,13 +104,13 @@ def plot_episode_reward_vs_time(env='CartPole-v1', n_episodes=50,
     X, random_rews, gen_rews = [], [], []
 
     def add_single_data(policy, policy_name, lst):
-        rew = util.rollout_total_reward(policy, env, n_episodes=n_episodes)
+        rew = util.rollout.total_reward(policy, env, n_episodes=n_episodes)
         lst.append(rew)
-        print("{} reward:".format(policy_name), rew)
+        logging.info("{} reward:".format(policy_name), rew)
 
     def make_data():
         X.append(trainer.epochs_so_far)
-        print("Epoch {}".format(trainer.epochs_so_far))
+        loggin.info("Epoch {}".format(trainer.epochs_so_far))
         # add_single_data(expert_policy, "expert", expert_rews)
         add_single_data(random_policy, "random", random_rews)
         add_single_data(gen_policy, "generator", gen_rews)
@@ -127,93 +127,17 @@ def plot_episode_reward_vs_time(env='CartPole-v1', n_episodes=50,
     _savefig_timestamp("plot_episode_reward_vs_time")
 
 
-def plot_discriminator_loss(env='CartPole-v1', n_steps_per_plot=1000,
-        n_plots=100, n_gen_warmup_steps=500):
-    """
-    Train the generator briefly, and then
-
-    Train the discriminator to distinguish (unchanging) expert rollouts versus
-    the unchanging random rollouts for a long time and plot discriminator loss.
-    """
-    trainer = init_trainer(env)
-    n_timesteps = len(trainer.expert_obs_old)
-    (gen_old_obs, gen_act, gen_new_obs, _) = util.rollout_generate(
-            trainer.policy, trainer.env, n_timesteps=n_timesteps)
-    kwargs = dict(gen_old_obs=gen_old_obs, gen_act=gen_act,
-            gen_new_obs=gen_new_obs)
-    trainer.train_gen(n_steps=n_gen_warmup_steps)
-
-    steps_so_far = 0
-    def epoch():
-        nonlocal steps_so_far
-        trainer.train_disc(**kwargs, n_steps=n_steps_per_plot)
-        steps_so_far += n_steps_per_plot
-
-    X = []
-    Y = []
-    def add_plot():
-        X.append(steps_so_far)
-        Y.append(trainer.eval_disc_loss(**kwargs))
-        print("step: {}".format(steps_so_far))
-        print("loss: {}".format(Y[-1]))
-
-    add_plot()
-    for _ in tqdm.tnrange(n_plots, desc="discriminator"):
-        epoch()
-        add_plot()
-
-    plt.plot(X, Y, label="discriminator loss")
-    plt.legend()
-    _savefig_timestamp("plot_discriminator_loss")
-
-
-def plot_generator_loss(env='CartPole-v1', n_steps_per_plot=5000,
-        n_plots=100, n_disc_warmup_steps=100):
-    """
-    Train the discriminator briefly, and then
-
-    Train the generator to distinguish (unchanging) expert rollouts to
-    confuse the discriminator, and plot discriminator loss.
-    """
-    trainer = init_trainer(env)
-    n_timesteps = len(trainer.expert_obs_old)
-
-    (gen_old_obs, gen_act, gen_new_obs, _) = util.rollout_generate(
-            trainer.policy, trainer.env, n_timesteps=n_timesteps)
-
-    steps_so_far = 0
-    def epoch():
-        nonlocal steps_so_far
-        trainer.train_gen(n_steps=n_steps_per_plot)
-        steps_so_far += n_steps_per_plot
-
-    X = []
-    Y = []
-    def add_plot():
-        X.append(steps_so_far)
-        Y.append(trainer.eval_disc_loss())
-        print("step: {}".format(steps_so_far))
-        print("disc loss: {}".format(Y[-1]))
-
-    add_plot()
-    for _ in tqdm.tnrange(n_plots, desc="generator"):
-        epoch()
-        add_plot()
-
-    plt.plot(X, Y, label="discriminator loss")
-    plt.legend()
-    _savefig_timestamp("plot_generator_loss")
-
-
 def plot_fight_loss(env='CartPole-v1',
-        n_epochs=100,
+        n_epochs=500,
         n_plots_each_per_epoch=10,
-        n_disc_steps_per_plot=500,
-        n_gen_steps_per_plot=50000,
-        n_rollout_samples=1000,
-        n_gen_plot_episodes=100,
+        n_disc_steps_per_plot=10,
+        n_gen_steps_per_plot=10000,
+        n_rollout_samples=4000,
+        n_gen_plot_episodes=10,
         trainer_hook_fn=None,
-        trainer=None):
+        trainer=None,
+        interactive=True,
+        ):
     """
     Alternate between training the generator and discriminator.
 
@@ -223,9 +147,9 @@ def plot_fight_loss(env='CartPole-v1',
     - Plot the performance of the generator policy versus the performance of
       a random policy.
     """
-    trainer = trainer or init_trainer(env, n_expert_samples=n_rollout_samples)
-    trainer_hook_fn(trainer)
-    n_timesteps = len(trainer.expert_obs_old)
+    trainer = trainer or init_trainer(env, n_expert_timesteps=n_rollout_samples)
+    if trainer_hook_fn:
+        trainer_hook_fn(trainer)
 
     plot_idx = 0
     def epoch(gen_mode=False):
@@ -247,8 +171,8 @@ def plot_fight_loss(env='CartPole-v1',
         X, Y = gen_data if gen_mode else disc_data
         X.append(plot_idx)
         Y.append(trainer.eval_disc_loss())
-        print("plot idx ({}): {}".format(mode, plot_idx), end=" ")
-        print("disc loss: {}".format(Y[-1]))
+        logging.info("plot idx ({}): {} disc loss: {}"
+                .format(mode, plot_idx, Y[-1]))
     def show_plot_disc():
         plt.scatter(disc_data[0], disc_data[1], c='g', alpha=0.7, s=4,
                 label="discriminator loss (dis step)")
@@ -256,23 +180,23 @@ def plot_fight_loss(env='CartPole-v1',
                 label="discriminator loss (gen step)")
         plt.title("epoch={}".format(epoch_num))
         plt.legend()
-        _savefig_timestamp("plot_fight_loss_disc")
+        _savefig_timestamp("plot_fight_loss_disc", interactive)
 
     gen_ep_reward = []
     rand_ep_reward = []
     def add_plot_gen():
         env_vec = util.make_vec_env(env, 8)
-        gen_policy = trainer.policy
+        gen_policy = trainer.gen_policy
         rand_policy = util.make_blank_policy(env)
 
-        gen_rew = util.rollout_total_reward(gen_policy, env,
+        gen_rew = util.rollout.total_reward(gen_policy, env,
                 n_episodes=n_gen_plot_episodes)/n_gen_plot_episodes
-        rand_rew = util.rollout_total_reward(rand_policy, env,
+        rand_rew = util.rollout.total_reward(rand_policy, env,
                 n_episodes=n_gen_plot_episodes)/n_gen_plot_episodes
         gen_ep_reward.append(gen_rew)
         rand_ep_reward.append(rand_rew)
-        print("generator reward:", gen_rew)
-        print("random reward:", rand_rew)
+        logging.info("generator reward: {}".format(gen_rew))
+        logging.info("random reward: {}".format(rand_rew))
     def show_plot_gen():
         plt.title("Cartpole performance (expert=500)")
         plt.xlabel("epochs")
@@ -281,11 +205,11 @@ def plot_fight_loss(env='CartPole-v1',
         plt.plot(gen_ep_reward, label="avg gen ep reward", c="red")
         plt.plot(rand_ep_reward, label="avg random ep reward", c="black")
         plt.legend()
-        _savefig_timestamp("plot_fight_epreward_gen")
+        _savefig_timestamp("plot_fight_epreward_gen", interactive)
 
     add_plot_disc(False)
     add_plot_gen()
-    for epoch_num in tqdm.tnrange(n_epochs, desc="epoch"):
+    for epoch_num in tqdm.trange(n_epochs, desc="epoch"):
         for _ in range(n_plots_each_per_epoch):
             epoch(False)
             add_plot_disc(False)
@@ -305,11 +229,14 @@ def plot_fight_loss(env='CartPole-v1',
 def _savefig_timestamp(prefix="", also_show=True):
     path = "output/{}_{}.png".format(prefix, datetime.datetime.now())
     plt.savefig(path)
-    plt.show()
+    logging.info("plot saved to {}".format(path))
+    if also_show:
+        plt.show()
 
 def _decor_tf_init(f):
     with tf.Session() as sess:
         pass
 
 if __name__ == "__main__":
-    plot_fight_loss()
+    logging.getLogger().setLevel(logging.INFO)
+    plot_fight_loss(interactive=False)
