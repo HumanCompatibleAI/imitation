9de69b33cb1f0f76067af5f1f142f7a5cadd26e2
Adam Gleave <adam@gleave.me>
2019-03-21 14:30:41 -0700

    Prototype CI config

---
+++ ci/code_checks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+SOURCE_DIRS="yairl"
+
+RET=0
+
+echo "flake8 --version"
+flake8 --version
+
+echo "Linting code"
+flake8 ${SOURCE_DIRS}
+RET=$(($RET + $?))
+
+echo "isort --version-number"
+isort --version-number
+
+echo "Checking import order using isort"
+isort --recursive --diff ${SOURCE_DIRS}
+isort --recursive --check-only ${SOURCE_DIRS}
+RET=$(($RET + $?))
+
+if [ $RET -ne 0 ]; then
+    echo "Linting failed."
+fi
+exit $RET
