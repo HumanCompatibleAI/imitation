4e57dc6db520e11fb42365d65b9839883101c9b8
Adam Gleave <adam@gleave.me>
2019-03-22 11:11:21 -0700

    Clean up package structure

--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='yairl',
     version='0.1',
-    packages=['yairl', ],
-    install_requires=['gym', 'numpy', 'tqdm', ],
-    extras_require = {
+    description='Implementation of modern IRL and imitation learning algorithms.',
+    author='Center for Human-Compatible AI and Google',
+    python_requires='>=3.6.0',
+    packages=find_packages(exclude=['test*']),
+    install_requires=[
+        'gin-config',
+        'gym',
+        'numpy',
+        'tensorflow',
+        'tqdm',
+        'stable-baselines',
+    ],
+    tests_require=[
+        'codecov',
+        'pytest',
+        'pytest-cov',
+    ],
+    extras_require={
         'gpu':  ['tensorflow-gpu'],
         'cpu': ['tensorflow']
     },
     url='https://github.com/HumanCompatibleAI/airl',
-    description='Implementation of modern IRL and imitation learning algorithms.',
-    author='Center for Human-Compatible AI and Google',
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
 )
