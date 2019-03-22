9796ba9f2279080b0c12590e7cd5844bca4de631
Adam Gleave <adam@gleave.me>
2019-03-21 17:13:50 -0700

    Fix imports

--- yairl/util/__init__.py
+++ yairl/util/__init__.py
@@ -2,6 +2,6 @@
 
 import yairl.util.rollout
 from yairl.util.util import (FeedForward32Policy, apply_ff, build_placeholders, flat, get_env_id,
-                             get_or_train_policy, is_vec_env, load_expert_policy, load_policy,
-                             load_trained_policy, make_blank_policy, make_save_policy_callback,
-                             make_vec_env, maybe_load_env, save_trained_policy)
+                             is_vec_env, load_expert_policy, load_policy, load_trained_policy,
+                             make_blank_policy, make_save_policy_callback, make_vec_env,
+                             maybe_load_env, save_trained_policy)
