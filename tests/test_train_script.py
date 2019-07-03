439edc51d72ac3403847ff100792ec21b00c14e3
Steven H. Wang <wang.steven.h@gmail.com>
2019-07-03 16:37:25 -0700

    Add a no crash test for scripts/train.py

---
+++ tests/test_train_script.py
@@ -0,0 +1,11 @@
+from imitation.scripts.train import train_and_plot
+
+
+def test_train_and_plot_no_crash():
+    train_and_plot(n_epochs=2,
+                   n_epochs_per_plot=1,
+                   n_disc_steps_per_epoch=1,
+                   n_gen_steps_per_epoch=1,
+                   n_episodes_per_reward_data=2,
+                   n_disc_samples_per_buffer=10,
+                   interactive=False)
