0f325701e6483c93ad28521ecc6ae52c04e7683a
Adam Gleave <adam@gleave.me>
2019-07-04 13:11:43 +0100

    Fix tests

--- tests/test_reward_net.py
+++ tests/test_reward_net.py
@@ -1,3 +1,4 @@
+import gym
 import pytest
 import tensorflow as tf
 
@@ -6,11 +7,12 @@ from imitation.reward_net import BasicRewardNet, BasicShapedRewardNet
 ENVS = ['FrozenLake-v0', 'CartPole-v1', 'Pendulum-v0']
 
 
-@pytest.mark.parametrize("env", ENVS)
-def test_init_no_crash(env):
+@pytest.mark.parametrize("env_id", ENVS)
+def test_init_no_crash(env_id):
+  env = gym.make(env_id)
   for i in range(3):
-    with tf.variable_scope(env + str(i) + "shaped"):
+    with tf.variable_scope(env_id + str(i) + "shaped"):
       BasicShapedRewardNet(env.observation_space, env.action_space)
-    with tf.variable_scope(env + str(i)):
+    with tf.variable_scope(env_id + str(i)):
       BasicRewardNet(env.observation_space, env.action_space)
   tf.reset_default_graph()
