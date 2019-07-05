e8c9d722ec253e5da262bf9d87ecea3ef1f11831
Steven H. Wang <wang.steven.h@gmail.com>
2019-07-05 11:49:32 -0700

    Add second key to test_buffer()

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -37,21 +37,23 @@ def test_buffer(capacity, chunk_len, sample_shape):
     FIFO insertion.
   * Mutating the inserted chunk shouldn't mutate the buffer.
   """
-  buf = Buffer(capacity, sample_shapes={'k': sample_shape}, dtypes={'k': float})
+  buf = Buffer(capacity,
+               sample_shapes={'a': sample_shape, 'b': sample_shape},
+               dtypes={'a': float, 'b': float})
 
   for i in range(0, capacity*3, chunk_len):
-    chunk = _fill_chunk(i, chunk_len, sample_shape)
     assert len(buf) == min(i, capacity)
     assert buf._idx == i % capacity
-    buf.store({'k': chunk})
-    samples = buf.sample(100)['k']
-    assert samples.shape == (100,) + sample_shape
-
-    _check_bound(i + chunk_len, capacity, samples)
+    chunk = _fill_chunk(i, chunk_len, sample_shape)
+    buf.store({'a': chunk, 'b': chunk})
+    for samples in buf.sample(100).values():
+      assert samples.shape == (100,) + sample_shape
+      _check_bound(i + chunk_len, capacity, samples)
 
     # Confirm that buffer is not mutable from inserted sample.
     chunk[:] = np.nan
-    assert not np.any(np.isnan(buf._arrays['k']))
+    assert not np.any(np.isnan(buf._arrays['a']))
+    assert not np.any(np.isnan(buf._arrays['b']))
 
 
 @pytest.mark.parametrize("capacity", [30, 60])
