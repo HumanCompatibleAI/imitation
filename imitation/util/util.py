7b49fc1a80c25c3e1b94c6ad34ad812621bbe46e
Adam Gleave <adam@gleave.me>
2019-07-18 10:13:45 +0100

    Add linting for common typos; fix typos in our code (#46)

--- imitation/util/util.py
+++ imitation/util/util.py
@@ -208,7 +208,7 @@ def load_policy(env, basedir="expert_models",
       base_dir (str): The directory of the pickled file.
       policy_network_class (stable_baselines.BasePolicy): A policy network
           constructor. Unless we are using a custom BasePolicy (not builtin to
-          stable_baselines), this is automatically infered, and so we can leave
+          stable_baselines), this is automatically inferred, and so we can leave
           this argument as None.
       **kwargs: Additional options for initializing the BaseRLModel class.
   """
