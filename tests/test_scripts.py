e4c7081008ac99e3e70feedfb48e2c2487f842f1
Adam Gleave <adam@gleave.me>
2019-07-20 10:37:11 +0100

    Isolate test output

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -6,12 +6,17 @@ from imitation.scripts.train import train_ex
 
 def test_policy_eval():
   """Smoke test for imitation.scripts.policy_eval"""
-  run = policy_eval_ex.run(config_updates={'render': False},
+  config_updates = {
+      'render': False,
+      'log_root': 'output/tests/policy_eval',
+  }
+  run = policy_eval_ex.run(config_updates=config_updates,
                            named_configs=['fast'])
   assert run.status == 'COMPLETED'
   assert isinstance(run.result, dict)
 
 
 def test_train():
-  train_ex.run(named_configs=['cartpole', 'gail', 'debug'])
+  train_ex.run(config_updates={'log_root': 'output/tests/train'},
+               named_configs=['cartpole', 'gail', 'debug'])
   tf.reset_default_graph()
