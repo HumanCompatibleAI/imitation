83b84b9609586c1c7950d5809d251b472b555e11
Adam Gleave <adam@gleave.me>
2019-07-20 14:49:55 +0100

    Remove xfailing/skipped tests

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -60,91 +60,10 @@ def test_train_disc_improve_D(use_gail, env='CartPole-v1', n_timesteps=200,
 
 
 @pytest.mark.expensive
-@pytest.mark.xfail(reason="(AIRL) With random seeding, this test passed 36 "
-                   "times out of 40.",
-                   raises=AssertionError)
-def test_train_gen_degrade_D(use_gail=False, env='CartPole-v1', n_timesteps=200,
-                             n_steps=10000):
-  trainer = init_trainer(env, use_gail=use_gail)
-  if use_gail:
-    kwargs = {}
-  else:
-    obs_old, act, obs_new, _ = rollout.generate_transitions(
-        trainer.gen_policy, env, n_timesteps=n_timesteps)
-    kwargs = dict(gen_old_obs=obs_old, gen_act=act, gen_new_obs=obs_new)
-
-  loss1 = trainer.eval_disc_loss(**kwargs)
-  trainer.train_gen(n_steps=n_steps)
-  loss2 = trainer.eval_disc_loss(**kwargs)
-  assert loss2 > loss1
-
-
-@pytest.mark.expensive
-@pytest.mark.xfail(reason="(AIRL) With random seeding, this test passed 19 "
-                   "times out of 30.",
-                   raises=AssertionError)
-def test_train_disc_then_gen(use_gail=False, env='CartPole-v1', n_timesteps=200,
-                             n_steps=10000):
-  trainer = init_trainer(env, use_gail=use_gail)
-  if use_gail:
-    kwargs = {}
-  else:
-    obs_old, act, obs_new, _ = rollout.generate_transitions(
-        trainer.gen_policy, env, n_timesteps=n_timesteps)
-    kwargs = dict(gen_old_obs=obs_old, gen_act=act, gen_new_obs=obs_new)
-
-  loss1 = trainer.eval_disc_loss(**kwargs)
-  trainer.train_disc(n_steps=n_steps, **kwargs)
-  loss2 = trainer.eval_disc_loss(**kwargs)
-  trainer.train_gen(n_steps=n_steps)
-  loss3 = trainer.eval_disc_loss(**kwargs)
-  assert loss2 < loss1
-  assert loss3 > loss2
-
-
-@pytest.mark.expensive
 @pytest.mark.parametrize("use_gail", USE_GAIL)
 def test_train_no_crash(use_gail, env='CartPole-v1'):
   trainer = init_trainer(env, use_gail=use_gail)
   trainer.train(n_epochs=1)
-
-
-@pytest.mark.expensive
-@pytest.mark.xfail(
-    reason="Either AIRL train is broken or not enough epochs."
-    " Consider making a plot of episode reward over time to check.",
-    raises=AssertionError)
-@pytest.mark.skip
-def test_trained_policy_better_than_random(use_gail, env='CartPole-v1',
-                                           n_episodes=50):
-  """
-  Make sure that generator policy trained to mimic expert policy
-  demonstrations) achieves higher reward than a random policy.
-
-  In other words, perform a basic check on the imitation learning
-  capabilities of AIRL and GAIL.
-  """
-  env = util.make_vec_env(env, 32)
-  trainer = init_trainer(env, use_random_expert=True, use_gail=use_gail)
-  expert_policy = util.load_policy(env, basedir="expert_models")
-  random_policy = util.make_blank_policy(env)
-  if expert_policy is None:
-    pytest.fail("Couldn't load expert_policy!")
-
-  trainer.train(n_epochs=200)
-
-  # Idea: Plot n_epochs vs generator reward.
-  for _ in range(4):
-    expert_ret = rollout.mean_return(expert_policy, env, n_episodes=n_episodes)
-    gen_ret = rollout.mean_return(trainer.gen_policy, env,
-                                  n_episodes=n_episodes)
-    random_ret = rollout.mean_return(random_policy, env, n_episodes=n_episodes)
-
-    print("expert return:", expert_ret)
-    print("generator return:", gen_ret)
-    print("random return:", random_ret)
-    assert expert_ret > random_ret
-    assert gen_ret > random_ret
 
 
 @pytest.mark.expensive
