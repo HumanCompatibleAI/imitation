40df56d9482a7fd0ad8f8b67c6f060e98192f25e
Steven H. Wang <wang.steven.h@gmail.com>
2019-07-23 11:42:00 -0700

    test_envs: Rm parallel param, use env_name param

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -16,18 +16,14 @@ except ImportError:
 
 @pytest.mark.skipif(not MUJOCO_OK,
                     reason="Requires `mujoco_py`, which isn't installed.")
-@pytest.mark.parametrize("parallel", PARALLEL)
-def test_envs(parallel):
+@pytest.mark.parametrize("env_name", ENV_NAMES)
+def test_envs(env_name):
   """Check that our custom environments don't crash on `step`, and `reset`."""
   for env_name in ENV_NAMES:
-    if parallel:
-      env = make_vec_env(env_name)
-    else:
-      env = gym.make(env_name)
-
-      env.reset()
-      obs_space = env.observation_space
-      for _ in range(4):
-        act = env.action_space.sample()
-        obs, rew, done, info = env.step(act)
-        assert obs in obs_space
+    env = gym.make(env_name)
+    env.reset()
+    obs_space = env.observation_space
+    for _ in range(4):
+      act = env.action_space.sample()
+      obs, rew, done, info = env.step(act)
+      assert obs in obs_space
