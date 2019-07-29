10f56306437a84e65b908b98b36bda29f1c4bc78
Steven H. Wang <wang.steven.h@gmail.com>
2019-07-29 16:32:50 -0700

    mujoco_experts.sh: Use `gdate` on macOS

--- experiments/mujoco_experts.sh
+++ experiments/mujoco_experts.sh
@@ -3,8 +3,14 @@
 ENVS="acrobot cartpole mountain_car reacher half_cheetah hopper ant humanoid"
 SEEDS="0 1 2"
 
-TIMESTAMP=$(date --iso-8601=seconds)
-OUTPUT_DIR=outputs/mujoco_experts/${TIMESTAMP}/
+if $(command -v gdate > /dev/null); then
+  DATE_CMD=gdate  # macOS compatibility
+else
+  DATE_CMD=date
+fi
+
+TIMESTAMP=$(${DATE_CMD} --iso-8601=seconds)
+OUTPUT_DIR=output/mujoco_experts/${TIMESTAMP}/
 
 parallel -j 25% --header : --results ${OUTPUT_DIR}/parallel/ \
          python -m imitation.scripts.data_collect with log_root=${OUTPUT_DIR} {env} seed={seed} \
