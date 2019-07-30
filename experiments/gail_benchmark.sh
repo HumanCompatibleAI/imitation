c4d829a1e9031a059526288285a1d7caf5470a2c
Steven H. Wang <wang.steven.h@gmail.com>
2019-07-30 15:35:44 -0700

    gail_benchmark.sh: Address review comments

--- experiments/gail_benchmark.sh
+++ experiments/gail_benchmark.sh
@@ -3,15 +3,14 @@
 # This script finds the the mean and standard error of episode return after
 # training GAIL on benchmark tasks.
 #
-# The CSV can be modified and regenerated from following spreadsheet:
+# The benchmark tasks are defined in the CSV config file
+# `experiments/gail_benchmark_config.csv`.
+#
+# The CSV configuration can be modified and regenerated from following spreadsheet:
 # https://docs.google.com/spreadsheets/d/1MZYTr23ddwhO2PrNI2EalKleoULXlsH5y9oqawt22pY/edit?usp=sharing
 #
-# When regenerating csv, remember that each line needs to end with ',' to
+# NOTE: When regenerating csv, remember that each line needs to end with ',' to
 # work properly.
-#
-# NOTE: Since I don't have walker experts available right now, I also
-# deleted the walker entries from the csv. TODO(shwang): Add them back in
-# by regenerating the csv after these experts are available.
 
 if $(command -v gdate > /dev/null); then
   DATE_CMD=gdate  # macOS compatibility
@@ -20,22 +19,24 @@ else
 fi
 
 TIMESTAMP=$(${DATE_CMD} --iso-8601=seconds)
+
 OUTPUT_DIR="output/gail_benchmark/${TIMESTAMP}"
 mkdir -p "${OUTPUT_DIR}"
+echo "Logging to: ${OUTPUT_DIR}"
 
 SEEDS="0 1 2"
 
 parallel -j 25% --header : --results ${OUTPUT_DIR}/parallel/ --colsep , --progress \
   python -m imitation.scripts.train \
   with \
-  fast \
+  "$@" \
   gail \
   {env_config} \
   log_root="${OUTPUT_DIR}" \
   init_trainer_kwargs.rollout_glob=expert_models/rollouts/{rollout_glob} \
   init_trainer_kwargs.n_expert_demos={n_demonstrations} \
   seed={seed} \
-  :::: experiments/gail_benchmark.csv ::: seed ${SEEDS}
+  :::: experiments/gail_benchmark_config.csv ::: seed ${SEEDS}
 
 
 # Directory path is really long. Enter the directory to shorten results output,
