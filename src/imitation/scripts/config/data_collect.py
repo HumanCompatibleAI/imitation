04c970e351e2cf3426cf344b8bdf35080f39ce7f
Adam Gleave <adam@gleave.me>
2019-07-31 21:04:45 +0100

    Humanoid and Ant expert (#66)
    * Default to checkpointing policies (smaller, more general) than rollouts; still save rollouts at end of training
    
    * Increase batch size for Ant and Humanoid

--- src/imitation/scripts/config/data_collect.py
+++ src/imitation/scripts/config/data_collect.py
@@ -17,12 +17,12 @@ def data_collect_defaults():
   normalize = True  # Use VecNormalize
   make_blank_policy_kwargs = dict(DEFAULT_BLANK_POLICY_KWARGS)
 
-  rollout_save_interval = 100  # Num updates between saves (<=0 disables)
+  rollout_save_interval = -1  # Num updates between saves (<=0 disables)
   rollout_save_final = True  # If True, save after training is finished.
   rollout_save_n_timesteps = 2000  # Min timesteps saved per file, optional.
   rollout_save_n_episodes = None  # Num episodes saved per file, optional.
 
-  policy_save_interval = -1  # Num updates between saves (<=0 disables)
+  policy_save_interval = 100  # Num updates between saves (<=0 disables)
   policy_save_final = True  # If True, save after training is finished.
 
   log_root = os.path.join("output", "data_collect")  # output directory
@@ -37,6 +37,9 @@ def logging(env_name, log_root):
 @data_collect_ex.named_config
 def ant():
   env_name = "Ant-v2"
+  make_blank_policy_kwargs = dict(
+      n_steps=2048,  # batch size of 2048*8=16384 due to num_vec
+  )
   total_timesteps = int(5e6)  # OK after 2e6, but continues improving
 
 
@@ -60,6 +63,9 @@ def halfcheetah():
 @data_collect_ex.named_config
 def humanoid():
   env_name = "Humanoid-v2"
+  make_blank_policy_kwargs = dict(
+      n_steps=2048,  # batch size of 2048*8=16384 due to num_vec
+  )
   total_timesteps = int(10e6)  # fairly discontinuous, needs at least 5e6
 
 
