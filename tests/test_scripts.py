9844af386aaeaee0eefce80d5c3199781a3af172
Steven H. Wang <wang.steven.h@gmail.com>
2019-08-05 16:35:51 -0700

    test_scripts: Write all outputs to tempdirs
    Prevents tests from using persistant disk space

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -15,10 +15,13 @@ from imitation.scripts.train import train_ex
 
 def test_data_collect_main():
   """Smoke test for imitation.scripts.data_collect.rollouts_and_policy"""
-  run = data_collect_ex.run(
-      named_configs=['cartpole', 'fast'],
-  )
-  assert run.status == 'COMPLETED'
+  with tempfile.TemporaryDirectory(prefix='imitation-data_collect-main',
+                                   ) as tmpdir:
+      run = data_collect_ex.run(
+          named_configs=['cartpole', 'fast'],
+          log_root=tmpdir,
+      )
+      assert run.status == 'COMPLETED'
 
 
 def test_data_collect_rollouts_from_policy():
@@ -29,7 +32,8 @@ def test_data_collect_rollouts_from_policy():
         command_name="rollouts_from_policy",
         named_configs=['cartpole', 'fast'],
         config_updates=dict(
-          rollout_save_dir=tmpdir,
+          log_root=tmpdir,
+          rollout_save_dir=osp.join(tmpdir, "rollouts"),
           policy_path="expert_models/PPO2_CartPole-v1_0",
         ))
   assert run.status == 'COMPLETED'
@@ -37,29 +41,33 @@ def test_data_collect_rollouts_from_policy():
 
 def test_policy_eval():
   """Smoke test for imitation.scripts.policy_eval"""
-  config_updates = {
-      'render': False,
-      'log_root': 'output/tests/policy_eval',
-  }
-  run = policy_eval_ex.run(config_updates=config_updates,
-                           named_configs=['fast'])
-  assert run.status == 'COMPLETED'
-  assert isinstance(run.result, dict)
+  with tempfile.TemporaryDirectory(prefix='imitation-policy_eval',
+                                   ) as tmpdir:
+      config_updates = {
+          'render': False,
+          'log_root': tmpdir,
+      }
+      run = policy_eval_ex.run(config_updates=config_updates,
+                               named_configs=['fast'])
+      assert run.status == 'COMPLETED'
+      assert isinstance(run.result, dict)
 
 
 def test_train():
   """Smoke test for imitation.scripts.train"""
-  config_updates = {
-      'init_trainer_kwargs': {
-          'rollout_glob': "tests/data/rollouts/CartPole*.pkl",
-      },
-      'log_root': 'output/tests/train',
-  }
-  run = train_ex.run(
-      named_configs=['cartpole', 'gail', 'fast'],
-      config_updates=config_updates,
-  )
-  assert run.status == 'COMPLETED'
+  with tempfile.TemporaryDirectory(prefix='imitation-train',
+                                   ) as tmpdir:
+      config_updates = {
+          'init_trainer_kwargs': {
+              'rollout_glob': "tests/data/rollouts/CartPole*.pkl",
+          },
+          'log_root': tmpdir,
+      }
+      run = train_ex.run(
+          named_configs=['cartpole', 'gail', 'fast'],
+          config_updates=config_updates,
+      )
+      assert run.status == 'COMPLETED'
 
 
 def test_transfer_learning():
