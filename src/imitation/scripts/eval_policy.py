0fb6dac4ffb177747f2cd9c551aec64ec9d83dec
Adam Gleave <adam@gleave.me>
2019-08-20 11:37:59 +0100

    Update eval_policy to use new load_reward interface

--- src/imitation/scripts/eval_policy.py
+++ src/imitation/scripts/eval_policy.py
@@ -1,3 +1,4 @@
+import contextlib
 import os.path as osp
 import time
 from typing import Optional
@@ -80,14 +81,16 @@ def eval_policy(_seed: int,
     venv = InteractiveRender(venv, render_fps)
   # TODO(adam): add support for videos using VideoRecorder?
 
-  if reward_type is not None:
-    reward_fn = load_reward(reward_type, reward_path, venv)
-    venv = reward_wrapper.RewardVecEnvWrapper(venv, reward_fn)
-    tf.logging.info(
-        f"Wrapped env in reward {reward_type} from {reward_path}.")
+  with contextlib.ExitStack() as stack:
+    if reward_type is not None:
+      reward_fn_ctx = load_reward(reward_type, reward_path, venv)
+      reward_fn = stack.enter_context(reward_fn_ctx)
+      venv = reward_wrapper.RewardVecEnvWrapper(venv, reward_fn)
+      tf.logging.info(
+          f"Wrapped env in reward {reward_type} from {reward_path}.")
 
-  with serialize.load_policy(policy_type, policy_path, venv) as policy:
-    stats = rollout.rollout_stats(policy, venv, n_timesteps=timesteps)
+    with serialize.load_policy(policy_type, policy_path, venv) as policy:
+      stats = rollout.rollout_stats(policy, venv, n_timesteps=timesteps)
 
   return stats
 
