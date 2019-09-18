3bfa07d7115a47da8d19b1aae142078a2f90a036
Adam Gleave <adam@gleave.me>
2019-09-18 12:11:18 +0100

    Use tmpdir fixture

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -6,7 +6,6 @@ named_config for each experiment implicitly sets parallel=False.
 """
 
 import os.path as osp
-import tempfile
 
 import pytest
 
@@ -15,31 +14,28 @@ from imitation.scripts.expert_demos import expert_demos_ex
 from imitation.scripts.train_adversarial import train_ex
 
 
-def test_expert_demos_main():
+def test_expert_demos_main(tmpdir):
   """Smoke test for imitation.scripts.expert_demos.rollouts_and_policy"""
-  with tempfile.TemporaryDirectory(prefix='imitation-data_collect-main',
-                                   ) as tmpdir:
-      run = expert_demos_ex.run(
-          named_configs=['cartpole', 'fast'],
-          config_updates=dict(
-            log_root=tmpdir,
-          ),
-      )
-      assert run.status == 'COMPLETED'
+  run = expert_demos_ex.run(
+      named_configs=['cartpole', 'fast'],
+      config_updates=dict(
+        log_root=tmpdir,
+      ),
+  )
+  assert run.status == 'COMPLETED'
 
 
-def test_expert_demos_rollouts_from_policy():
+def test_expert_demos_rollouts_from_policy(tmpdir):
   """Smoke test for imitation.scripts.expert_demos.rollouts_from_policy"""
-  with tempfile.TemporaryDirectory(prefix='imitation-data_collect-policy',
-                                   ) as tmpdir:
-    run = expert_demos_ex.run(
-        command_name="rollouts_from_policy",
-        named_configs=['cartpole', 'fast'],
-        config_updates=dict(
-          log_root=tmpdir,
-          rollout_save_dir=osp.join(tmpdir, "rollouts"),
-          policy_path="expert_models/PPO2_CartPole-v1_0",
-        ))
+  run = expert_demos_ex.run(
+      command_name="rollouts_from_policy",
+      named_configs=['cartpole', 'fast'],
+      config_updates=dict(
+        log_root=tmpdir,
+        rollout_save_dir=osp.join(tmpdir, "rollouts"),
+        policy_path="expert_models/PPO2_CartPole-v1_0",
+      ),
+  )
   assert run.status == 'COMPLETED'
 
 
@@ -50,67 +46,61 @@ EVAL_POLICY_CONFIGS = [
 
 
 @pytest.mark.parametrize('config', EVAL_POLICY_CONFIGS)
-def test_eval_policy(config):
+def test_eval_policy(config, tmpdir):
   """Smoke test for imitation.scripts.eval_policy"""
-  with tempfile.TemporaryDirectory(prefix='imitation-policy_eval',
-                                   ) as tmpdir:
-      config_updates = {
-          'render': False,
-          'log_root': tmpdir,
-      }
-      config_updates.update(config)
-      run = eval_policy_ex.run(config_updates=config_updates,
-                               named_configs=['fast'])
-      assert run.status == 'COMPLETED'
-      assert isinstance(run.result, dict)
+  config_updates = {
+      'render': False,
+      'log_root': tmpdir,
+  }
+  config_updates.update(config)
+  run = eval_policy_ex.run(config_updates=config_updates,
+                           named_configs=['fast'])
+  assert run.status == 'COMPLETED'
+  assert isinstance(run.result, dict)
 
 
-def test_train_adversarial():
+def test_train_adversarial(tmpdir):
   """Smoke test for imitation.scripts.train_adversarial"""
-  with tempfile.TemporaryDirectory(prefix='imitation-train',
-                                   ) as tmpdir:
-      config_updates = {
-          'init_trainer_kwargs': {
-              # Rollouts are small, decrease size of buffer to avoid warning
-              'trainer_kwargs': {
-                  'n_disc_samples_per_buffer': 50,
-              },
+  config_updates = {
+      'init_trainer_kwargs': {
+          # Rollouts are small, decrease size of buffer to avoid warning
+          'trainer_kwargs': {
+              'n_disc_samples_per_buffer': 50,
           },
-          'log_root': tmpdir,
-          'rollout_glob': "tests/data/rollouts/CartPole*.pkl",
-      }
-      run = train_ex.run(
-          named_configs=['cartpole', 'gail', 'fast'],
-          config_updates=config_updates,
-      )
-      assert run.status == 'COMPLETED'
+      },
+      'log_root': tmpdir,
+      'rollout_glob': "tests/data/rollouts/CartPole*.pkl",
+  }
+  run = train_ex.run(
+      named_configs=['cartpole', 'gail', 'fast'],
+      config_updates=config_updates,
+  )
+  assert run.status == 'COMPLETED'
 
 
-def test_transfer_learning():
+def test_transfer_learning(tmpdir):
   """Transfer learning smoke test.
 
   Save a dummy AIRL test reward, then load it for transfer learning."""
 
-  with tempfile.TemporaryDirectory(prefix='imitation-transfer',
-                                   ) as tmpdir:
-    log_dir_train = osp.join(tmpdir, "train")
-    run = train_ex.run(
-        named_configs=['cartpole', 'airl', 'fast'],
-        config_updates=dict(
-          rollout_glob="tests/data/rollouts/CartPole*.pkl",
-          log_dir=log_dir_train,
-        ),
-    )
-    assert run.status == 'COMPLETED'
+  log_dir_train = osp.join(tmpdir, "train")
+  run = train_ex.run(
+      named_configs=['cartpole', 'airl', 'fast'],
+      config_updates=dict(
+        rollout_glob="tests/data/rollouts/CartPole*.pkl",
+        log_dir=log_dir_train,
+      ),
+  )
+  assert run.status == 'COMPLETED'
 
-    log_dir_data = osp.join(tmpdir, "expert_demos")
-    discrim_path = osp.join(log_dir_train, "checkpoints", "final", "discrim")
-    run = expert_demos_ex.run(
-        named_configs=['cartpole', 'fast'],
-        config_updates=dict(
-          log_dir=log_dir_data,
-          reward_type='DiscrimNet',
-          reward_path=discrim_path,
-        ),
-    )
-    assert run.status == 'COMPLETED'
+  log_dir_data = osp.join(tmpdir, "expert_demos")
+  discrim_path = osp.join(log_dir_train, "checkpoints", "final", "discrim")
+  run = expert_demos_ex.run(
+      named_configs=['cartpole', 'fast'],
+      config_updates=dict(
+        log_dir=log_dir_data,
+        reward_type='DiscrimNet',
+        reward_path=discrim_path,
+      ),
+  )
+  assert run.status == 'COMPLETED'
