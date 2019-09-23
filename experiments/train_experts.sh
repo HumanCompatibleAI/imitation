4895e3d3cf3fadaed96387dfe96e502e49f3d4bc
Steven H. Wang <wang.steven.h@gmail.com>
2019-09-23 12:58:32 -0700

    train_experts.sh: Stop generating a zip file

--- experiments/train_experts.sh
+++ experiments/train_experts.sh
@@ -32,44 +32,11 @@ parallel -j 25% --header : --progress --results ${OUTPUT_DIR}/parallel/ \
   ::: env ${ENVS} ::: seed ${SEEDS}
 
 pushd $OUTPUT_DIR
-shopt -s failglob  # Catch obvious errors by reporting glob match fails.
 
 # Display and save mean episode reward to ${RESULTS_FILE}.
 find . -name stdout | xargs tail -n 15 | grep -E '(==|ep_reward_mean)' | tee ${RESULTS_FILE}
 
-# For easy zip archiving into the correct format, build symlinks to the
-# first experts corresponding to each ${env_name}.
-tmp_dir=$(mktemp -d "${TMPDIR:-/tmp}/train_experts.XXX")
-
-for env_name in */; do
-  env_name=$(echo ${env_name} | sed -e 's|/$||')  # Remove trailing slash
-  if [ $env_name == "parallel" ]; then
-    # Not actually an env name; this is a log dir for the parallel command.
-    continue
-  fi
-
-  pushd ${env_name} > /dev/null
-  mkdir -p ${tmp_dir}/${env_name}
-
-  for uuid in */; do
-    uuid=$(echo ${uuid} | sed -e 's|/$||')  # Remove trailing slash
-    pushd ${uuid} > /dev/null
-    ln -s "$(pwd)/policies/final" "${tmp_dir}/${env_name}/${uuid}"
-    popd > /dev/null
-  done
-
-  popd > /dev/null
-done
-
-# Build zipfile using the directory structure corresponding to the symlinks
-# from before.
-ZIP_FILE=$(pwd)/experts.zip
-zip -v ${ZIP_FILE} ${RESULTS_FILE}
-
-pushd ${tmp_dir}
-zip -rv ${ZIP_FILE} */
 popd
-echo "Expert zip saved to ${ZIP_FILE}"
 
-shopt -u failglob
-popd
+echo "[Optional] Upload new experts to S3 (replacing old ones) using:"
+echo "aws s3 sync --delete '${OUTPUT_DIR}' s3://shwang-chai/public/expert_models"
