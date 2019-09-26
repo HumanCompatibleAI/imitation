779f4196fa2a5f1997491aa56b5988b4212b022e
Steven H. Wang <wang.steven.h@gmail.com>
2019-09-26 16:20:05 -0700

    WIP

---
+++ src/imitation/util/sacred.py
@@ -0,0 +1,70 @@
+# Copyright 2019 Google LLC.
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+import os
+from typing import Dict, List
+import warning
+
+
+class SacredDicts(NamedTuple):
+  """Each dict `foo` is loaded from `f"{sacred_dir}/foo.json"`."""
+  sacred_dir: str
+  config: dict
+  result: dict
+  run: dict
+
+  @classmethod
+  def load_from_dir(cls, sacred_dir: str):
+    args = []
+    for field in SacredDicts._fields:
+      if field == "sacred_dir":
+        args.append(dir)
+      else:
+        json_path = os.path.join(dir, "sacred", f"{field}.json")
+        if not os.path.is_file(json_path):
+          args.append({})
+          if field != "result":  # All other {field}.json are guaranteed.
+            warning.warn(
+              RuntimeWarning(f"Couldn't load {json_path}. Skipping."))
+        else:
+          with open(config_path, "r") as f:
+            args.append(json.load(f))
+    return cls(*args)
+
+
+def dir_contains_metrics_json(dir_path: str) -> bool:
+  metrics_path = os.path.join(dir_path, "metrics.json")
+  return os.path.isfile(metrics_path)
+
+
+def filter_subdirs(
+  root_dir: str,
+  filter_fn: Callable[[str], bool] = dir_contains_metrics_json,
+  *,
+  nested_ok: bool = False,
+) -> List[str]:
+  """Walks through a directory tree, return paths to filtered directories.
+
+  Args:
+    root_dir: The start of the directory tree walk.
+    filter_fn: A function with takes a directory path and returns True if
+      we should include the directory path in this function's return value.
+    nested_ok: If False, then error if in the return value, one of the
+      directory paths is a subdirectory of another.
+  Returns:
+    A list of all subdirectory paths where `filter_fn(path) == True`.
+  """
+  filtered_dirs = set()
+  for root, _, _ in os.walk(root_dir):
+    if filter_fn(root):
+      filtered_dirs.add(root)
+
+  if not nested_ok:
+    for dirpath in exper_dirs:
+      components = os.path.split(dirpath)
+      for i in range(1, len(components)):
+        prefix = os.path.join(*components[0:i])
+        if prefix in expert_dirs:
+          raise ValueError(f"Parent {prefix} to {dir} also a dir directory")
+  return list(filtered_dirs)
