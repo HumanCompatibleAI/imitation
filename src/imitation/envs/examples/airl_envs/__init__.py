abcf30a6c1db819fc7054284f7aae089f94dedf9
Adam Gleave <adam@gleave.me>
2019-10-26 13:29:48 -0700

    imitation/PointMaze: Optionally add velocity to observations so that environment is actually approximately Markovian

--- src/imitation/envs/examples/airl_envs/__init__.py
+++ src/imitation/envs/examples/airl_envs/__init__.py
@@ -10,23 +10,24 @@ def _register(env_name: str, entry_point: str, kwargs: Optional[dict] = None):
     gym_register(id=env_name, entry_point=entry_point, kwargs=kwargs)
 
 
+def _point_maze_register():
+  for dname, dval in {'Left': 0, 'Right': 1}.items():
+    for vname, vval in {'': False, 'Vel': True}.items():
+      _register(f'imitation/PointMaze{dname}{vname}-v0',
+                entry_point='point_maze_env:PointMazeEnv',
+                kwargs={
+                  'direction': dval,
+                  'include_vel': vval,
+                })
+
+
 _register('imitation/ObjPusher-v0',
           entry_point=f'pusher_env:PusherEnv',
           kwargs={'sparse_reward': False})
 _register('imitation/TwoDMaze-v0',
           entry_point='twod_maze:TwoDMaze')
-_register('imitation/PointMazeRight-v0',
-          entry_point='point_maze_env:PointMazeEnv',
-          kwargs={
-              'sparse_reward': False,
-              'direction': 1,
-          })
-_register('imitation/PointMazeLeft-v0',
-          entry_point='point_maze_env:PointMazeEnv',
-          kwargs={
-              'sparse_reward': False,
-              'direction': 0,
-          })
+
+_point_maze_register()
 
 # A modified ant which flips over less and learns faster via TRPO
 _register('imitation/CustomAnt-v0',
