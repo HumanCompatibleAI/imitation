947f35a46161a306374a33c789cbfa4cf89e4df4
Steven H. Wang <wang.steven.h@gmail.com>
2019-12-30 22:51:27 -0800

    Add TransitionsRecordingWrapper

---
+++ src/imitation/util/buffering_wrapper.py
@@ -0,0 +1,58 @@
+import numpy as np
+from imitation.util import rollout
+from stable_baselines.common.vec_env import VecEnv, VecEnvWrapper
+
+class BufferingWrapper(VecEnvWrapper):
+  """Saves transitions of underlying VecEnv.
+
+  Retrieve saved transitions using `pop_transitions()`.
+  """
+
+  def __init__(self, venv: VecEnv, error_on_premature_reset: bool = True):
+    """
+    Args:
+      venv: The wrapped VecEnv.
+      error_premature_reset: Error if `reset()` is called on this wrapper
+        and there are saved samples that haven't yet been accessed.
+    """
+    super().__init__(venv)
+    self.error_on_premature_reset = error_on_premature_reset
+    self.obs_list = None
+    self.acts_list = None
+
+  def reset(self, **kwargs):
+    if self.error_on_premature_reset and len(self.acts_list) != 0:
+      raise RuntimeError("TransitionsRecordingWrapper reset() before samples "
+                         "were accessed")
+    obs = self.venv.reset(**kwargs)
+    self.obs_list = [obs]
+    self.acts_list = []
+    return obs
+
+  def step(self, actions):
+    obs, rews, dones, infos = self.venv.step_wait()
+    real_obs = np.copy(obs)
+    for i, done in enumerate(dones):
+      if done:
+        real_obs[i] = infos[i]['terminal_observation']
+    self.obs_list.append(real_obs)
+    self.acts_list.append(actions)
+    assert len(self.obs_list) == len(self.acts_list) + 1
+    return obs, rews, dones, infos
+
+  def pop_transitions(self) -> rollout.Transitions:
+    """Pops recorded transitions, returning them as an instance of Transitions.
+    """
+    assert len(self.obs_list) == len(self.acts_list) + 1
+    obs = np.stack(self.obs_list[:-1])
+    acts = np.stack(self.acts_list)
+    new_obs = np.stack(self.obs_list[1:])
+
+    self.obs_list = [self.obs_list[-1]]
+    self.acts_list = []
+
+    return rollout.Transitions(obs=obs, acts=acts, new_obs=new_obs)
+
+  def step_wait(self):
+    return self.venv.step_wait()
+
---
+++ tests/test_buffering_wrapper.py
@@ -0,0 +1,94 @@
+import gym
+import numpy as np
+import pytest
+
+from stable_baselines.common.vec_env import DummyVecEnv
+from imitation.util.test_buffer_wrapper import BufferingWrapper
+
+
+class CountingEnv(gym.Env):
+  """At timestep `t`, has `reward == obs == t`."""
+
+  def __init__(self):
+    self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=())
+    self.action_space = gym.spaces.Box(low=0, high=np.inf, shape=())
+    self.timestep = None
+
+  def reset(self):
+    self.timestep = 0
+
+  def step(self, action):
+    assert self.timestep is not None
+    assert action in self.action_space
+    return self.timestep, self.timestep, False, {}
+
+
+def _make_recording_venv(error_on_premature_reset: bool,
+                         ) -> BufferingWrapper:
+  venv = DummyVecEnv([CountingEnv] * 2)
+  venv = BufferingWrapper(venv, error_on_premature_reset)
+  venv.reset()
+  return venv
+
+
+def test_buffering_wrapper_pop_transitions():
+  """Check transitions_pop() result for BufferWrapper.
+
+  To make things easy to check, we use a dummy environment where the observation
+  is simply the timestep.
+  """
+  venv = _make_recording_venv
+  for _ in range(3):
+    expect_obs_list = [0]
+    expect_acts_list = []
+
+    obs = venv.reset()
+    np.testing.assert_array_equal(obs, [0, 0])
+    expect_obs_list.append(obs)
+
+    for t in range(1, 10):
+      acts = np.random.random(size=(2,))
+      expect_acts_list.append(acts)
+
+      obs, *_ = venv.step(acts)
+      np.testing.assert_array_equal(obs, [t, t])
+      expect_obs_list.append(obs)
+
+    samples = venv.pop_transitions()
+    np.testing.assert_array_equal(samples.obs, expect_obs_list[:-1])
+    np.testing.assert_array_equal(samples.acts, expect_acts_list)
+    np.testing.assert_array_equal(samples.new_obs, expect_obs_list[1:])
+
+
+def test_vec_env_recording_error():
+  # Resetting before a `step()` is okay.
+  for flag in [True, False]:
+    venv = _make_recording_venv(flag)
+    for _ in range(10):
+      venv.reset()
+
+  # Resetting after a `step()` is not okay if error flag is True.
+  venv = _make_recording_venv(True)
+  venv.step()
+  with pytest.raises(RuntimeError, matches="TransitionsRecordingWrapper"):
+    venv.reset()
+
+  # Same as previous case, but insert a `pop_transitions()` in between.
+  venv = _make_recording_venv(True)
+  venv.step()
+  venv.pop_transitions()
+  venv.step()
+  with pytest.raises(RuntimeError, matches="TransitionsRecordingWrapper"):
+    venv.reset()
+
+  # Resetting after a `step()` is ok if error flag is False.
+  venv = _make_recording_venv(False)
+  venv.step()
+  venv.reset()
+
+  # Resetting after a `step()` is ok if transitions are first collected.
+  for flag in [True, False]:
+    venv = _make_recording_venv(flag)
+    venv.step()
+    venv.pop_transitions()
+    venv.reset()
