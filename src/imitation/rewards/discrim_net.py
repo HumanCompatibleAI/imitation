discrim_net: Fix accidental AIRL no-op softmax (#147)
* discrim_net: Fix accidental AIRL no-op softmax

* discrim_net: Use more direct approach

src/imitation/rewards/discrim_net.py
@@ -237,20 +237,17 @@ class DiscrimNetAIRL(DiscrimNet):
         labels=self.labels_ph,
         logits=self._presoftmax_disc_logits,
         name="discrim_loss",
-    )
-
-    self._policy_test_reward = self.reward_net.reward_output_test
+    )  # (None,)
 
     # Construct generator reward:
     # \[\hat{r}(s,a) = \log(D_{\theta}(s,a)) - \log(1 - D_{\theta}(s,a)).\]
     # This simplifies to:
     # \[\hat{r}(s,a) = f_{\theta}(s,a) - \log \pi(a \mid s).\]
     # This is just an entropy-regularized objective.
-    self._log_D = tf.nn.log_softmax(self.reward_net.reward_output_train)
-    self._log_D_compl = tf.nn.log_softmax(self.log_policy_act_prob_ph)
-    # Note self._log_D_compl is effectively an entropy term.
-    ent_bonus = self.entropy_weight * self._log_D_compl
-    self._policy_train_reward = self._log_D - ent_bonus
+
+    ent_bonus = -self.entropy_weight * self.log_policy_act_prob_ph
+    self._policy_train_reward = self.reward_net.reward_output_train + ent_bonus
+    self._policy_test_reward = self.reward_net.reward_output_test
 
     self.reward_net.build_summaries()
 