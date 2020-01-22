e7fc11db74abbc7bfafda14590a642d6b3d8840a
Steven H. Wang <wang.steven.h@gmail.com>
2020-01-21 18:43:52 -0800

    Update src/imitation/util/util.py
    Co-Authored-By: Adam Gleave <adam@gleave.me>

--- src/imitation/util/util.py
+++ src/imitation/util/util.py
@@ -71,7 +71,7 @@ def make_vec_env(env_name: str,
 
     if max_episode_steps is not None:
       env = TimeLimit(env, max_episode_steps)
-    elif (spec.max_episode_steps is not None) and not spec.tags.get('vnc'):
+    elif spec.max_episode_steps is not None:
       env = TimeLimit(env, max_episode_steps=spec.max_episode_steps)
 
     # Use Monitor to record statistics needed for Baselines algorithms logging
