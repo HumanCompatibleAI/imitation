539a08a2eaafc0d98d65ef523cf45786db6bdbae
Steven H. Wang <wang.steven.h@gmail.com>
2020-03-19 14:17:30 -0700

    fix indent

--- experiments/convert_traj.py
+++ experiments/convert_traj.py
@@ -27,24 +27,24 @@ def convert_trajs_to_sb(trajs: List[rollout.Trajectory]) -> dict:
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("src_path", type=str)
-    parser.add_argument("dst_path", type=str)
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("src_path", type=str)
+  parser.add_argument("dst_path", type=str)
+  args = parser.parse_args()
 
-    src_path = Path(args.src_path)
-    dst_path = Path(args.dst_path)
+  src_path = Path(args.src_path)
+  dst_path = Path(args.dst_path)
 
-    assert src_path.is_file()
-    with open(src_path, "rb") as f:
-      src_trajs = pickle.load(f)  # type: List[rollout.Trajectory]
+  assert src_path.is_file()
+  with open(src_path, "rb") as f:
+    src_trajs = pickle.load(f)  # type: List[rollout.Trajectory]
 
-    dst_trajs = convert_trajs_to_sb(src_trajs)
-    os.makedirs(dst_path.parent, exist_ok=True)
-    with open(dst_path, "wb") as f:
-      np.savez_compressed(f, **dst_trajs)
+  dst_trajs = convert_trajs_to_sb(src_trajs)
+  os.makedirs(dst_path.parent, exist_ok=True)
+  with open(dst_path, "wb") as f:
+    np.savez_compressed(f, **dst_trajs)
 
-    print(f"Dumped rollouts to {dst_path}")
+  print(f"Dumped rollouts to {dst_path}")
 
 
 if __name__ == "__main__":
