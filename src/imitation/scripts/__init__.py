6f82a1b69ffebb26bd5d2c0c2a0188c02875622c
Steven H. Wang <wang.steven.h@gmail.com>
2020-03-19 14:21:56 -0700

    scripts.__init__: Disable pytype ImportError
    Errors when benchmark_environments isn't installed even though
    we've wrapped in try-catch

--- src/imitation/scripts/__init__.py
+++ src/imitation/scripts/__init__.py
@@ -1,7 +1,9 @@
 # Add our custom environments to Gym registry.
 
 try:
+  # pytype: disable=import-error
   import benchmark_environments.classic_control  # noqa: F401
+  # pytype: enable=import-error
 except ImportError:
   pass
 
