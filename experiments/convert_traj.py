f6e19c27d8a05ad02586533ad6ce4f39e902d40f
Steven H. Wang <wang.steven.h@gmail.com>
2020-03-19 13:54:29 -0700

    Address review

--- experiments/convert_traj.py
+++ experiments/convert_traj.py
@@ -3,17 +3,21 @@
 Convert trajectories from `imitation` format to openai/baselines GAIL format.
 """
 
+import argparse
+import os
+from pathlib import Path
+import pickle
 from typing import List
 
 import numpy as np
 
-from imitation.util import rollout as ro_util
+from imitation.util import rollout
 
 
-def convert_trajs_to_sb(trajs: List[ro_util.Trajectory]) -> dict:
+def convert_trajs_to_sb(trajs: List[rollout.Trajectory]) -> dict:
   """Converts Trajectories into the dict format used by Stable Baselines GAIL.
   """
-  trans = ro_util.flatten_trajectories(trajs)
+  trans = rollout.flatten_trajectories(trajs)
   return dict(
     acs=trans.acts,
     rews=trans.rews,
@@ -22,11 +26,7 @@ def convert_trajs_to_sb(trajs: List[ro_util.Trajectory]) -> dict:
   )
 
 
-if __name__ == "__main__":
-    import argparse
-    import os
-    from pathlib import Path
-    import pickle
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("src_path", type=str)
     parser.add_argument("dst_path", type=str)
@@ -37,11 +37,15 @@ if __name__ == "__main__":
 
     assert src_path.is_file()
     with open(src_path, "rb") as f:
-      src_trajs = pickle.load(f)
+      src_trajs = pickle.load(f)  # type: List[rollout.Trajectory]
 
-    dst_trajs = convert_trajs_to_sb(src_trajs)  # type: ro_util.Trajectory
+    dst_trajs = convert_trajs_to_sb(src_trajs)
     os.makedirs(dst_path.parent, exist_ok=True)
     with open(dst_path, "wb") as f:
       np.savez_compressed(f, **dst_trajs)
 
     print(f"Dumped files to {dst_path}")
+
+
+if __name__ == "__main__":
+  main()
