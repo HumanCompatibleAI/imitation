1ab2859b59a6b1ce28d8581fbbc762c6c65c28bf
Steven H. Wang <wang.steven.h@gmail.com>
2020-03-23 22:53:15 -0700

    Address comments

--- experiments/mountain_car_plots.py
+++ experiments/mountain_car_plots.py
@@ -1,6 +1,6 @@
 """Heatmaps and reward plotting code for debugging MountainCar."""
 
-from functools import partial
+import functools
 import os
 from pathlib import Path
 import pickle
@@ -9,8 +9,6 @@ from typing import Dict, List, Optional, Sequence, Union
 import gym
 from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
 
 from imitation import util
@@ -21,6 +19,47 @@ from imitation.util.reward_wrapper import RewardFn
 MC_POS_MIN, MC_POS_MAX = -1.2, 0.6
 MC_VEL_MIN, MC_VEL_MAX = -0.07, 0.07
 MC_GOAL_POS = 0.5
+MC_NUM_ACTS = 3
+
+
+def _reward_fn_normalize_inputs(obs: np.ndarray,
+                                acts: np.ndarray,
+                                next_obs: np.ndarray,
+                                steps: Optional[np.ndarray] = None,
+                                *,
+                                reward_fn: RewardFn,
+                                vec_normalize: VecNormalize,
+                                norm_reward: bool = True,
+                                verbose: bool = True,
+                                ) -> np.ndarray:
+  """Combine with `functools.partial` to create an input-normalizing RewardFn.
+
+  Args:
+    reward_fn: The reward function that normalized inputs are evaluated on.
+    vec_normalize: Instance of VecNormalize used to normalize inputs and
+     rewards.
+    norm_reward: If True, then also normalize reward before returning.
+  Returns:
+    The possibly normalized reward.
+  """
+  norm_obs = vec_normalize.norm_obs(obs)
+  norm_next_obs = vec_normalize.norm_obs(next_obs)
+  rew = reward_fn(norm_obs, acts, norm_next_obs, steps)
+  if norm_reward:
+    rew = vec_normalize.normalize_reward(rew)
+  if verbose:
+    print("rew normed min:", rew.min())
+    print("rew normed max:", rew.max())
+  return rew
+
+
+def build_norm_reward_fn(*, reward_fn, vec_normalize, **kwargs) -> RewardFn:
+  """Reward function that automatically normalizes inputs.
+
+  See _reward_fn_normalize_inputs for argument documentation.
+  """
+  return functools.partial(_reward_fn_normalize_inputs, reward_fn=reward_fn,
+                           vec_normalize=vec_normalize, **kwargs)
 
 
 # Utility for calculating the next observation s'.
@@ -37,8 +76,8 @@ def make_next_obs(obs, acts) -> np.ndarray:
   for ob, act in zip(obs, acts):
     assert ob.shape == (2,)
     env.reset()
-    env.env.state = ob
-    next_ob, *_ = env.step(act)
+    env.unwrapped.state = ob
+    next_ob = env.step(act)[0]
     next_obs.append(next_ob)
   return np.array(next_obs)
 
@@ -56,8 +95,8 @@ def make_policy_rollout(policy_path, env_name="MountainCar-v0",
 def make_heatmap(
     act: int,
     reward_fn: RewardFn,
-    pos_step_size: float = 0.1,
-    velocity_step_size: float = 1e-2,
+    n_pos_step: int = 18,
+    n_vel_step: int = 14,
     mark_goal: bool = True,
     gen_trajs: Optional[List[util.rollout.Trajectory]] = None,
     exp_trajs: Optional[List[util.rollout.Trajectory]] = None,
@@ -65,7 +104,7 @@ def make_heatmap(
     title: bool = None,
     heatmap: bool = True,
     filter_trans_by_act: bool = True,
-) -> None:
+) -> plt.Figure:
   """
   Make a MountainCar heatmap of rewards for a particular action. X axis
   is position. Y axis is velocity.
@@ -74,9 +113,10 @@ def make_heatmap(
     act: The MountainCar action number whose reward we are evaluating.
       Should be 0, 1, or 2.
     reward_fn: Reward function. Should accept unnormalized inputs.
-    pos_step_size: The x axis granularity.
-    velocity_step_size: The y axis granularity.
-    mark_goal: If True then plot a line for goal position.
+    n_pos_step: The number of squares that the x axis of the heatmap is divided
+      into.
+    n_vel_step: The number of squares that the y axis of the heatmap is divided
+      into.
     gen_trajs: A list of generator trajectories to
       scatterplot on top of the heatmap.
     exp_trajs: A list of exp trajectories to scatterplot on
@@ -88,61 +128,50 @@ def make_heatmap(
       `gen_trajs` and `exp_trajs` that don't use action `act` before
       scatterplotting.
   """
-  assert 0 <= act <= 2
+  assert 0 <= act < MC_NUM_ACTS
 
-  obs_vec, acts_vec = [], []
-  for p in np.arange(MC_POS_MIN, MC_POS_MAX + 1e-5, step=pos_step_size):
-    for v in np.arange(MC_VEL_MIN, MC_VEL_MAX + 1e-5, step=velocity_step_size):
-      obs_vec.append([p, v])
-      acts_vec.append(act)
+  pos_space = np.linspace(MC_POS_MIN, MC_POS_MAX, n_pos_step, endpoint=True)
+  vel_space = np.linspace(MC_VEL_MIN, MC_VEL_MAX, n_vel_step, endpoint=True)
 
-  obs_vec = np.array(obs_vec)
-  acts_vec = np.array(acts_vec)
+  obs_vec = np.array([[p, v] for p in pos_space for v in vel_space])
+  acts_vec = np.array([act] * len(obs_vec))
   next_obs_vec = make_next_obs(obs_vec, acts_vec)
+  steps = np.arange(len(acts_vec))
 
-  R = reward_fn(obs_vec, acts_vec, next_obs_vec, None)
+  rew = reward_fn(obs_vec, acts_vec, next_obs_vec, steps)
+  rew_matrix = rew.reshape(n_pos_step, n_vel_step)
 
-  df = pd.DataFrame()
-  df["position"] = np.round(obs_vec[:, 0], 5)
-  df["velocity"] = np.round(obs_vec[:, 1], 5)
-  df["reward"] = R
-  df = df.pivot("velocity", "position", "reward")
-
-  def convert_to_rc(pos, vel):
-    x = (np.array(pos) - MC_POS_MIN) / pos_step_size + 0.5
-    y = (np.array(vel) - MC_VEL_MIN) / velocity_step_size + 0.5
-    return x, y
-
-  def convert_to_rc_filtered(trajs):
+  def convert_traj_to_coords_filtered(trajs):
     trans = util.rollout.flatten_trajectories(trajs)
     obs = trans.obs
     if filter_trans_by_act:
       obs = obs[trans.acts == act]
-    pos, vel = obs[:, 0], obs[:, 1]
-    return convert_to_rc(pos, vel)
+    return obs[:, 0], obs[:, 1]
 
-  plt.figure()
+  fig, ax = plt.subplots()
   if heatmap:
-    sns.heatmap(df)
+    c = ax.pcolor(pos_space, vel_space, rew_matrix)
+    fig.colorbar(c, ax=ax)
 
   if mark_goal:
-    goal_r, _ = convert_to_rc(MC_GOAL_POS, 0)
-    plt.axvline(x=goal_r, linestyle='--',
-                label=f"goal state (pos={MC_GOAL_POS})")
+    ax.axvline(x=MC_GOAL_POS, linestyle='--',
+               label=f"goal state (pos={MC_GOAL_POS})")
   if exp_trajs is not None:
-    X, Y = convert_to_rc_filtered(exp_trajs)
-    plt.scatter(X, Y, marker="o", label="expert samples", alpha=0.2)
+    X, Y = convert_traj_to_coords_filtered(exp_trajs)
+    ax.scatter(X, Y, marker="o", label="expert samples", alpha=0.2)
   if gen_trajs is not None:
-    X, Y = convert_to_rc_filtered(gen_trajs)
-    plt.scatter(X, Y, marker="o", c="yellow", label="policy samples",
-                alpha=0.2)
+    X, Y = convert_traj_to_coords_filtered(gen_trajs)
+    ax.scatter(X, Y, marker="o", c="yellow", label="policy samples",
+               alpha=0.2)
 
   act_names = ["left", "neutral", "right"]
   if title is None:
     title = f"Action {act_names[act]}"
-  plt.title(title)
+  ax.set_title(title)
   if legend_on:
-    plt.legend(loc="center left", bbox_to_anchor=(0, 1.3))
+    ax.legend(loc="center left", bbox_to_anchor=(0, 1.3))
+
+  return fig
 
 
 def batch_reward_heatmaps(
@@ -171,17 +200,21 @@ def batch_reward_heatmaps(
     discrim_path = checkpoint_dir / "discrim"
     policy_path = checkpoint_dir / "gen_policy"
 
+    # Automatically loads VecNormalize for policy evaluation.
+    # `gen_trajs` contains unnormalized observations.
+    gen_trajs = make_policy_rollout(policy_path)
+
+    # Load VecNormalize for use in RewardFn, which doesn't automatically
+    # normalize input observations.
     with open(vec_normalize_path, "rb") as f:
       vec_normalize = pickle.load(f)  # type: VecNormalize
     vec_normalize.training = False
 
-    gen_trajs = make_policy_rollout(policy_path)
-
-    reward_fn_ctx = load_reward("DiscrimNet", discrim_path, venv="unused")
+    reward_fn_ctx = load_reward("DiscrimNet", discrim_path, venv=None)
     with reward_fn_ctx as reward_fn:
       norm_rew_fn = build_norm_reward_fn(reward_fn=reward_fn,
                                          vec_normalize=vec_normalize)
-      for i in range(3):
+      for i in range(MC_NUM_ACTS):
         make_heatmap(act=i, reward_fn=norm_rew_fn, gen_trajs=gen_trajs,
                      exp_trajs=exp_trajs)
         save_path = Path(output_dir, f"act_{i}", checkpoint_dir.name)
@@ -200,7 +233,7 @@ def plot_reward_vs_time(
     trajs_dict: Dict[str, List[util.rollout.Trajectory]],
     reward_fn: RewardFn,
     colors: Sequence[str] = ("tab:blue", "tab:orange"),
-):
+) -> plt.Figure:
   """Plots rewards received by many trajectories from many agents over time.
 
   Args:
@@ -209,61 +242,22 @@ def plot_reward_vs_time(
       reward_fn: Reward function for evaluating rollout rewards.
       colors: Custom colors for plotted rewards.
   """
-  plt.clf()
+  fig, ax = plt.subplots()
   for i, (traj_name, traj_list) in enumerate(trajs_dict.items()):
     X = []
     Y = []
     for traj in traj_list:
       T = len(traj.rews)
       X.extend(range(T))
-      rews = reward_fn(traj.obs[:-1], traj.acts, traj.obs[1:], None)
+      steps = np.arange(len(traj.acts))
+      rews = reward_fn(traj.obs[:-1], traj.acts, traj.obs[1:], steps)
       Y.extend(rews)
     if i < len(colors):
       color = colors[i]
     else:
       color = None
-    plt.plot(X, Y, alpha=1.0, c=color, label=traj_name)
-  plt.xlabel("timestep")
-  plt.ylabel("test reward")
-  plt.legend()
-  plt.show()
-
-
-def _reward_fn_normalize_inputs(obs: np.ndarray,
-                                acts: np.ndarray,
-                                next_obs: np.ndarray,
-                                steps: Optional[np.ndarray] = None,
-                                *,
-                                reward_fn: RewardFn,
-                                vec_normalize: VecNormalize,
-                                norm_reward: bool = True,
-                                verbose: bool = True,
-                                ) -> np.ndarray:
-  """Combine with `partial` to create an input-normalizing RewardFn.
-
-  Args:
-    reward_fn: The reward function that normalized inputs are evaluated on.
-    vec_normalize: Instance of VecNormalize used to normalize inputs and
-     rewards.
-    norm_reward: If True, then also normalize reward before returning.
-  Returns:
-    The possibly normalized reward.
-  """
-  norm_obs = vec_normalize.norm_obs(obs)
-  norm_next_obs = vec_normalize.norm_obs(next_obs)
-  rew = reward_fn(norm_obs, acts, norm_next_obs, steps)
-  if norm_reward:
-    rew = vec_normalize.normalize_reward(rew)
-  if verbose:
-    print("rew normed min:", rew.min())
-    print("rew normed max:", rew.max())
-  return rew
-
-
-def build_norm_reward_fn(*, reward_fn, vec_normalize, **kwargs) -> RewardFn:
-  """Reward function that automatically normalizes inputs.
-
-  See _reward_fn_normalize_inputs for argument documentation.
-  """
-  return partial(_reward_fn_normalize_inputs, reward_fn=reward_fn,
-                 vec_normalize=vec_normalize, **kwargs)
+    ax.plot(X, Y, alpha=1.0, c=color, label=traj_name)
+  ax.xlabel("timestep")
+  ax.ylabel("test reward")
+  ax.legend()
+  return fig
