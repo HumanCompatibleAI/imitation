3e750c414a22d7ccb93a3c0b212abaf9af8674ca
Adam Gleave <adam@gleave.me>
2020-05-08 16:51:45 -0700

    Rename benchmark_environments to seals

--- src/imitation/scripts/__init__.py
+++ src/imitation/scripts/__init__.py
@@ -2,7 +2,7 @@
 
 try:
     # pytype: disable=import-error
-    import benchmark_environments.classic_control  # noqa: F401
+    import seals  # noqa: F401
 
     # pytype: enable=import-error
 except ImportError:
