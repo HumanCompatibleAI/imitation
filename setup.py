6c38e763fb12a67ceef86f97716aaf18543c056a
Steven H. Wang <wang.steven.h@gmail.com>
2020-06-01 14:11:29 -0700

    setup: Update Sacred TODO

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ setup(
         "jax~=0.1.66",
         "jaxlib~=0.1.47",
         # Install from IDSIA/sacred@master once
-        # https://github.com/IDSIA/sacred/pull/737 is merged.
+        # https://github.com/IDSIA/sacred/pull/737 and
+        # https://github.com/IDSIA/sacred/pull/740 are merged.
         "sacred @ git+https://github.com/HumanCompatibleAI/sacred@0.8.1-with-imit-compat",  # noqa: E501
     ],
     tests_require=TESTS_REQUIRE,
