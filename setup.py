824cefde5d83286e2b42c32fe045612a2c2699dc
Steven H. Wang <wang.steven.h@gmail.com>
2020-08-28 13:55:33 -0700

    Bump isort to 5.0 to reflect pyflake-isort install

--- setup.py
+++ setup.py
@@ -68,8 +68,7 @@ setup(
             "autopep8",
             "ntfy[slack]",
             "ipdb",
-            # isort 4.X required for flake8-isort (2020-07-07)
-            "isort~=4.0",
+            "isort~=5.0",
             "jupyter",
             "pytype",
             "codespell",
