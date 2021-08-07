1ef75dde20f197df28d7b227034e1342599c6251
Steven H. Wang <wang.steven.h@gmail.com>
2021-08-06 19:23:07 -0700

    scripts: Add script that updates old traj pickles

---
+++ src/imitation/scripts/convert_trajs_in_place.py
@@ -0,0 +1,42 @@
+"""
+This script takes as command-line input multiple paths to pickle files containing
+Sequence[imitation.types.TrajectoryWithRew] or Sequence[imitation.old_types.Trajectory].
+It overwrites each path with a new pickle where the demonstration data is saved as
+Sequence[TrajectoryWithRew].
+"""
+
+import warnings
+
+from imitation.data import types
+
+
+def update_traj_file_in_place(path: str) -> None:
+    """Modifies trajectories pickle file in-place to update data to new format.
+
+    Args:
+        path: Path to a pickle file containing `Sequence[imitation.types.Trajectory]`
+            or `Sequence[imitation.old_types.Trajectory`.
+    """
+    with warnings.catch_warnings():
+        # Filter out DeprecationWarning because we expect to load old trajectories here.
+        warnings.filterwarnings(
+            "ignore",
+            message=".*trajectories are saved in an outdated format.*",
+            category=DeprecationWarning,
+        )
+        trajs = types.load(path)
+    types.save(path, trajs)
+
+
+def main():
+    import sys
+
+    if len(sys.argv) <= 1:
+        print("Supply at least one path to pickled trajectories.")
+    else:
+        for path in sys.argv[1:]:
+            update_traj_file_in_place(path)
+
+
+if __name__ == "__main__":
+    main()
