43b840bc78374e1d2f9a746986b3786f85215415
Erik Jenner <erik.jenner99@gmail.com>
2021-08-23 17:34:59 +0200

    Fix logger error in test

--- tests/test_preference_comparisons.py
+++ tests/test_preference_comparisons.py
@@ -23,7 +23,9 @@ def test_trainer_no_crash():
         n_envs=1,
     )
     reward_net = BasicRewardNet(venv.observation_space, venv.action_space)
-    agent = PPO("MlpPolicy", venv)
+    # verbose=1 suppresses SB3 logger configuration,
+    # which conflicts with imitation logging
+    agent = PPO("MlpPolicy", venv, verbose=1)
     agent_trainer = AgentTrainer(agent, reward_net)
     fragmenter = RandomFragmenter(fragment_length=2, num_pairs=2, seed=0)
     trainer = PreferenceComparisons(agent_trainer, reward_net, 10, fragmenter)
