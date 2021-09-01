be5e248a70a1caab0d7616185ac8dfb6fd24c631
Adam Gleave <adam@gleave.me>
2021-09-01 12:49:07 -0700

    Fix code review

--- src/imitation/data/rollout.py
+++ src/imitation/data/rollout.py
@@ -362,9 +362,13 @@ def rollout_stats(trajectories: Sequence[types.TrajectoryWithRew]) -> Dict[str, 
     for t in trajectories:
         if t.infos is not None:
             ep_return = t.infos[-1].get("episode", {}).get("r")
-            if ep_return:
+            if ep_return is not None:
                 monitor_ep_returns.append(ep_return)
     if monitor_ep_returns:
+        # Note monitor_ep_returns[i] may be from a different episode than ep_return[i]
+        # since we skip episodes with None infos. This is OK as we only return summary
+        # statistics, but you cannot e.g. compute the correlation between ep_return and
+        # monitor_ep_returns.
         traj_descriptors["monitor_return"] = np.asarray(monitor_ep_returns)
         # monitor_return_len may be < n_traj when infos is sometimes missing
         out_stats["monitor_return_len"] = len(traj_descriptors["monitor_return"])
