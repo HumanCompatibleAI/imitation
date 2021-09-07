b482a6f95bfaa5e9738950d5ee74ccf6d9fd80f6
Adam Gleave <adam@gleave.me>
2021-09-07 11:11:50 -0700

    rollout new type migration: update test data, fix conversion script, add test coverage (#340)
    * Update convert_trajs (still useful as we have terminal backwards compatibility logic)
    
    * Add test case for convert_trajs
    
    * Update rollouts files
    
    * Clarify docstring

--- src/imitation/scripts/convert_trajs_in_place.py
+++ src/imitation/scripts/convert_trajs_in_place.py
@@ -1,8 +1,9 @@
 """
 This script takes as command-line input multiple paths to pickle files containing
-Sequence[imitation.types.TrajectoryWithRew] or Sequence[imitation.old_types.Trajectory].
-It overwrites each path with a new pickle where the demonstration data is saved as
-Sequence[imitation.types.TrajectoryWithRew].
+(possibly old forms of) Sequence[imitation.types.TrajectoryWithRew]. It overwrites each
+path with a new pickle where the demonstration data is saved as
+Sequence[imitation.types.TrajectoryWithRew]. This is updated to the newest format,
+setting `terminal=True` where it is missing.
 """
 
 import warnings
@@ -23,7 +24,7 @@ def update_traj_file_in_place(path: str) -> None:
         # Filter out DeprecationWarning because we expect to load old trajectories here.
         warnings.filterwarnings(
             "ignore",
-            message=".*trajectories are saved in an outdated format.*",
+            message="Loading old version of Trajectory.*",
             category=DeprecationWarning,
         )
         trajs = types.load(path)
