750fd52ee84b7d8e5dec383fc3e7a2a743a63ec1
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2021-09-10 02:05:30 -0700

    adding flag catcher

--- runners/build_push_image.sh
+++ runners/build_push_image.sh
@@ -1,23 +1,64 @@
 #!/bin/bash
-# Usage: build_push_image.sh <tag_names>
-# Running the following command will build and push the base image to Docker Hub 
-#        runners/build_push_image.sh base
-# Running the following command to build and push two stages
-#        runners/build_push_image.sh base python-req
 
-KEYS="$@"
+set -e # Exit immediately if a command exits with a non-zero status.
+
+__usage="build_push_image.sh - Building and pushing Docker image
+
+Usage: build_push_image.sh [options] [tags]
+
+options:
+  -h, --help                show brief help
+  -p, --push                push the image to DockerHub
+tags:
+  base                      base stage image
+  python-req                python-req stage image
+"
+
+KEYS=""
+PUSH=0
+
+while test $# -gt 0; do
+  case "$1" in
+  -p | --push)
+    PUSH=1 # Push the image to Docker Hub
+    shift
+    ;;
+  base)
+    KEYS+="base "
+    shift
+    ;;
+  python-req)
+    KEYS+="python-req "
+    shift
+    ;;
+  -h | --help)
+    echo "${__usage}"
+    exit 0
+    ;;
+  *)
+    echo "Unrecognized flag $1" >&2
+    exit 1
+    ;;
+  esac
+done
 
 if [[ -z $KEYS ]]; then
-    KEYS="base"
-    echo "No tag found in the arguments! Building and pushing default image humancompatibleai/imitation:${KEYS}"
+  KEYS="base"
+  echo "No tag found in the arguments! Building and pushing default image humancompatibleai/imitation:${KEYS}"
 fi
 
 for key in $KEYS; do
-    echo "----- Building and pushing humancompatibleai/imitation:${key} ..."
-    BUILD="docker build --target ${key} -t humancompatibleai/imitation:${key} ."
-    PUSH="docker push humancompatibleai/imitation:${key}"
-    echo "${BUILD}"
-    ${BUILD}
-    echo "${PUSH}"
-    # ${PUSH}  # Uncomment this line to push the image to Docker Hub 
+  echo "----- Building and pushing humancompatibleai/imitation:${key} ..."
+  BUILD_CMD="docker build --target ${key} -t humancompatibleai/imitation:${key} ."
+  PUSH_CMD="docker push humancompatibleai/imitation:${key}"
+
+  # Build image
+  # echo "${BUILD_CMD}"
+  ${BUILD_CMD}
+
+  # Push image if prompted
+  if [[ $PUSH == 1 ]]; then
+    # echo "${PUSH_CMD}"
+    ${PUSH_CMD}
+  fi
 done
