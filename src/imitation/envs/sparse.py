49bb4b4a3a76120ca801af5a916985a8d11e1199
Erik Jenner <erik.jenner99@gmail.com>
2021-09-22 15:00:18 +0200

    Increse sparse reacher tolerance

--- src/imitation/envs/sparse.py
+++ src/imitation/envs/sparse.py
@@ -8,7 +8,7 @@ from gym.envs.mujoco import reacher
 class SparseReacher(reacher.ReacherEnv):
     def step(self, a):
         vec = self.get_body_com("fingertip") - self.get_body_com("target")
-        reward = (np.linalg.norm(vec) < 0.01).astype(np.float32)
+        reward = (np.linalg.norm(vec) < 0.05).astype(np.float32)
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         done = False
