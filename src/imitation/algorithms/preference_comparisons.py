1e4bcc533dcc0c72a77aa6cac9abcbb3b70ccbb9
Yawen Duan <kmdanielduan@gmail.com>
2021-11-09 17:14:16 +0800

    saving policy periodically

--- src/imitation/algorithms/preference_comparisons.py
+++ src/imitation/algorithms/preference_comparisons.py
@@ -728,6 +728,7 @@ class PreferenceComparisons(base.BaseImitationAlgorithm):
         custom_logger: Optional[imit_logger.HierarchicalLogger] = None,
         allow_variable_horizon: bool = False,
         seed: Optional[int] = None,
+        vec_normalize: Optional[vec_env.VecNormalize] = None,
     ):
         """Initialize the preference comparison trainer.
 
@@ -772,6 +773,9 @@ class PreferenceComparisons(base.BaseImitationAlgorithm):
                 Only used when default components are used; if you instantiate your
                 own fragmenter, preference gatherer, etc., you are responsible for
                 seeding them!
+            vec_normalize: optional VecNormalize instance to use for normalizing. This
+                is used for saving the vec_normalize stats. If it's None, then saving
+                nothing when calling save_checkpoint callback.
         """
         super().__init__(
             custom_logger=custom_logger,
@@ -811,6 +815,8 @@ class PreferenceComparisons(base.BaseImitationAlgorithm):
         self.transition_oversampling = transition_oversampling
 
         self.dataset = PreferenceDataset()
+
+        self.vec_normalize = vec_normalize
 
     def train(
         self,
