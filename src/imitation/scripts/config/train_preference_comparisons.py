dcffab7c8a92d267a909a77b9c8a215376922ea6
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2021-11-24 18:10:57 -0800

    add weight decay in preference comparisons training config

--- src/imitation/scripts/config/train_preference_comparisons.py
+++ src/imitation/scripts/config/train_preference_comparisons.py
@@ -31,6 +31,7 @@ def train_defaults():
 
     reward_trainer_kwargs = {
         "epochs": 3,
+        "weight_decay": 0.0,
     }
     save_preferences = False  # save preference dataset at the end?
     agent_path = None  # path to a (partially) trained agent to load at the beginning
