2103b055bd07f835509096ff7abcd934882f7ba0
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2022-02-23 05:08:32 -0800

    refactor stay_prob, add trajectory_generator_kwargs

--- tests/policies/test_exploration_wrapper.py
+++ tests/policies/test_exploration_wrapper.py
@@ -11,7 +11,7 @@ def constant_policy(obs):
     return np.zeros(len(obs), dtype=int)
 
 
-def make_wrapper(random_prob, stay_prob):
+def make_wrapper(random_prob, switch_prob):
     venv = util.make_vec_env(
         "seals/CartPole-v0",
         n_envs=1,
@@ -21,27 +21,27 @@ def make_wrapper(random_prob, stay_prob):
             policy=constant_policy,
             venv=venv,
             random_prob=random_prob,
-            stay_prob=stay_prob,
+            switch_prob=switch_prob,
             seed=0,
         ),
         venv,
     )
 
 
-def test_switch():
-    wrapper, _ = make_wrapper(random_prob=0.0, stay_prob=0.5)
+def test_random_prob():
+    wrapper, _ = make_wrapper(random_prob=0.0, switch_prob=0.5)
     assert wrapper.current_policy == constant_policy
     for _ in range(100):
         wrapper._switch()
         assert wrapper.current_policy == constant_policy
 
-    wrapper, _ = make_wrapper(random_prob=1.0, stay_prob=0.5)
+    wrapper, _ = make_wrapper(random_prob=1.0, switch_prob=0.5)
     assert wrapper.current_policy == wrapper._random_policy
     for _ in range(100):
         wrapper._switch()
         assert wrapper.current_policy == wrapper._random_policy
 
-    wrapper, _ = make_wrapper(random_prob=0.5, stay_prob=0.5)
+    wrapper, _ = make_wrapper(random_prob=0.5, switch_prob=0.5)
     num_random = 0
     num_constant = 0
     for _ in range(1000):
@@ -61,7 +61,7 @@ def test_valid_output():
     # Ensure that we test both the random and the wrapped policy
     # at least once:
     for random_prob in [0.0, 0.5, 1.0]:
-        wrapper, venv = make_wrapper(random_prob=random_prob, stay_prob=0.5)
+        wrapper, venv = make_wrapper(random_prob=random_prob, switch_prob=0.5)
         np.random.seed(0)
         obs = np.random.rand(100, 2)
         for action in wrapper(obs):
