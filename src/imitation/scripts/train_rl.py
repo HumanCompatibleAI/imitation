1befb79a6305f66cc7bb27b633a995d5b2b6f96c
Maximilian Ernestus <maximilian@ernestus.de>
2022-02-24 17:25:29 +0100

    Fix bug when saving rollouts in train_rl.py

--- src/imitation/scripts/train_rl.py
+++ src/imitation/scripts/train_rl.py
@@ -124,7 +124,7 @@ def train_rl(
             rollout_save_n_timesteps,
             rollout_save_n_episodes,
         )
-        types.save(save_path, rollout.rollout(save_path, rl_algo, venv, sample_until))
+        types.save(save_path, rollout.rollout(rl_algo, venv, sample_until))
     if policy_save_final:
         output_dir = os.path.join(policy_dir, "final")
         serialize.save_stable_model(output_dir, rl_algo)
