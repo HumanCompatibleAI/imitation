a7d26aa4a34fdfcf171dba94865dbaae0eafe6dc
Maximilian Ernestus <maximilian@ernestus.de>
2022-02-24 10:45:11 +0100

    Fix formatting in quickstart.py

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,4 +1,6 @@
+"""This is a simple example demonstrating behavior cloning."""
 import pickle
+
 import gym
 import seals  # noqa: F401
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -9,8 +11,11 @@ from imitation.data import rollout
 env = gym.make("seals/CartPole-v0")
 with open(".pytest_cache/d/experts/CartPole-v1/rollout.pkl", "rb") as f:
     demonstrations = rollout.flatten_trajectories(pickle.load(f))
-bc_trainer = bc.BC(observation_space=env.observation_space, action_space=env.action_space,
-                   demonstrations=demonstrations)
+bc_trainer = bc.BC(
+    observation_space=env.observation_space,
+    action_space=env.action_space,
+    demonstrations=demonstrations,
+)
 
 print("Before behaviour cloning:")
 reward, _ = evaluate_policy(bc_trainer.policy, env, 3, render=True)
