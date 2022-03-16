cf2fee863007e8d755a1ce757f0c473f7ecd49b5
Yawen Duan <35738259+kmdanielduan@users.noreply.github.com>
2022-03-16 15:21:07 +0800

    Add `wandb_additional_info` in `wb.wandb_config()` (#417)
    * Creating WandbOutputFormat writer
    
    * adding wandb_config to common.py
    
    * add wandb to dependency list
    
    * move all wandb-related configs into new ingredient
    
    * remove changes to configure() in util.logger
    
    * add wb_ingredient to all train scripts and configs
    
    * add flag to enable wandb logging
    
    * making nested ingredient of wandb under common
    
    * dump with step=self.gen_algo.num_timesteps
    
    * remove unused dependency
    
    * change the script arg to invoke wandb
    
    * unformatting train_rl_ex
    
    * combine make_wandb_kwargs and wandb.init
    
    * reduce wb dependency, adjust adversarial
    
    * make sure total_timesteps is int
    
    * change experiment args
    
    * removing wb_commit
    
    * Moving wandb dependencies to whenever it's used
    
    * Set named_config to add wandb format_str
    
    * code format and style
    
    * modify docstring
    
    * Small bug fix in preference comparisons
    
    * Allow pretraining reward model
    
    * Support random trajectories in DRLHP
    
    * Slightly change default DRLHP config
    
    This is not yet tuned in any formal way, but it's what I used
    for most runs.
    
    * Rename initial_comparison_fraction
    
    Make it shorter and more consistent with random_frac.
    
    * Fix typo
    
    Need only agent_steps - avail_steps new transitions
    
    * Fix formatting
    
    * Add missing trailing commas
    
    * delete nohup.out
    
    * remove types from docstring
    
    * better exception capture
    
    * add comments for log_format_strs_additional
    
    * assert key match of key_values and key_excluded
    
    * Change log_format_strs_additional from list to dict to support concatenating multiple format strings
    
    * save reward model checkpoints
    
    * saving policy periodically
    
    * save policy only when trajectory_path is None
    
    * logging wrapped reward
    
    * Disable sample number warning by default
    
    In a synthetic setting, it makes sense to allow transitions
    to appear multiple times.
    
    * Train reward model longer if more pairs available
    
    * More detailed doctoring
    
    Co-authored-by: Adam Gleave <adam@gleave.me>
    
    * fix formats
    
    * remove vec_normalize argument and factoring save()
    
    * loading pre-trained RL policies
    
    * Revert "loading pre-trained RL policies"
    
    This reverts commit 84c0ef13dd78638e020b0441c1147ff044c700ed.
    
    * Revert "loading pre-trained RL policies"
    
    This reverts commit 84c0ef13dd78638e020b0441c1147ff044c700ed.
    
    * remove vec_normalize argument from saving callback
    
    * fix function naming and comments
    
    * fix testcase for prefernce comparisons
    
    * fix docstring length
    
    * adding weight decay choice for reward trainer
    
    * refactor buffering_wrapped to buffering_wrapped_venv
    
    * add weight decay in preference comparisons training config
    
    * mocking test for imitation.util.logger.WandbOutputFormat
    
    * Use MuJoCo-free context in CI
    
    * close log_obj
    
    * code format fix
    
    * simplify test file
    
    * simplify test file fix
    
    * cover MockWandb
    
    * reverse buffer_warapper refactoring, fix deleted code for exploration_frac by mistake
    
    * reverse refactoring
    
    * reverse refactor
    
    * refactor stay_prob
    
    * change stay_prob to switch_prob
    
    * add wandb_additional_info
    
    * add selection of parallel_method in common
    
    * refactor stay_prob, add trajectory_generator_kwargs
    
    * add test_switch_prob
    
    * fix code format
    
    * code format fix
    
    * remove redundant lines of code
    
    * remove parallel_method from make_vec_env()
    
    * recover parallel in fast()
    
    * make black happy
    
    * better descriptions of tests
    
    * simplify test_switch_prob()
    
    * add raise ValueError in docstrings
    
    * set default weight_decay
    
    * fix comment
    
    Co-authored-by: Adam Gleave <adam@gleave.me>
    
    * fix docstring
    
    Co-authored-by: Adam Gleave <adam@gleave.me>
    
    * docstring fix
    
    Co-authored-by: Erik Jenner <erik.jenner99@gmail.com>
    Co-authored-by: Adam Gleave <adam@gleave.me>
    Co-authored-by: Erik Jenner <erik@Eriks-MacBook-Air.local>

--- src/imitation/scripts/common/wb.py
+++ src/imitation/scripts/common/wb.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 @wandb_ingredient.config
 def wandb_config():
-    """Other user can overwrite this function to customize their wandb.init() call."""
+    # Other users can overwrite this function to customize their wandb.init() call.
     wandb_tag = None  # User-specified tag for this run
     wandb_name_prefix = ""  # User-specified prefix for the run name
     wandb_kwargs = dict(
@@ -19,6 +19,7 @@ def wandb_config():
         monitor_gym=False,
         save_code=False,
     )  # Other kwargs to pass to wandb.init()
+    wandb_additional_info = dict()
 
     locals()
 
@@ -29,6 +30,7 @@ def wandb_init(
     wandb_name_prefix: str,
     wandb_tag: Optional[str],
     wandb_kwargs: Mapping[str, Any],
+    wandb_additional_info: Mapping[str, Any],
     log_dir: str,
 ) -> None:
     """Putting everything together to get the W&B kwargs for wandb.init().
@@ -37,6 +39,8 @@ def wandb_init(
         wandb_name_prefix: User-specified prefix for wandb run name.
         wandb_tag: User-specified tag for this run.
         wandb_kwargs: User-specified kwargs for wandb.init().
+        wandb_additional_info: User-specific additional info to add to wandb experiment
+            ``config``.
         log_dir: W&B logs will be stored in directory `{log_dir}/wandb/`.
 
     Raises:
@@ -61,4 +65,6 @@ def wandb_init(
             "Trying to call `wandb.init()` but `wandb` not installed: "
             "try `pip install wandb`.",
         ) from e
-    wandb.init(config=_run.config, **updated_wandb_kwargs)
+    wandb_config_dict = dict(**_run.config)
+    wandb_config_dict.update(wandb_additional_info)
+    wandb.init(config=wandb_config_dict, **updated_wandb_kwargs)
