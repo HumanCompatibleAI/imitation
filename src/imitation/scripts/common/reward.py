d605f8b55ba92e16477806c69d6cbde4d19d98af
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2022-04-08 17:02:19 -0700

    Set default reward output norm

--- src/imitation/scripts/common/reward.py
+++ src/imitation/scripts/common/reward.py
@@ -54,7 +54,10 @@ def config_hook(config, command_name, logger):
     if "normalize_input_layer" not in config["reward"]["net_kwargs"]:
         res["net_kwargs"] = {"normalize_input_layer": networks.RunningNorm}
     if "normalize_output_layer" not in config["reward"]["net_kwargs"]:
-        res["net_kwargs"] = {"normalize_output_layer": None}
+        if command_name == "train_preference_comparisons":
+            res["net_kwargs"] = {"normalize_output_layer": networks.RunningNorm}
+        else:
+            res["net_kwargs"] = {"normalize_output_layer": None}
     return res
 
 
