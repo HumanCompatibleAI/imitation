a35778b74991f7d51b559ed7aa769ca7b1dc6211
Lev McKinney <levmckinney@gmail.com>
2022-07-13 18:31:57 -0700

    removed failure_rate.sh

--- failure_rate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-N=100
-for i in $(seq 1 $N); do
-    if ! pytest tests/algorithms/test_mce_irl.py -k test_mce_irl_reasonable_mdp; then failed=$(expr $failed + 1); fi
-done
-echo $failed / $N
