8cccb06efdb4acb47fd19dbad420ed44887800da
Juan Rocamonde <juancarlosrocamonde@gmail.com>
2022-07-15 10:05:15 +0100

    Update src/imitation/scripts/train_adversarial.py
    Co-authored-by: Adam Gleave <adam@gleave.me>

--- src/imitation/scripts/train_adversarial.py
+++ src/imitation/scripts/train_adversarial.py
@@ -77,7 +77,7 @@ def train_adversarial(
 
     Checkpoints:
         - AdversarialTrainer train and test RewardNets are saved to 
-           `f"{log_dir}/checkpoints/{step}/reward_train.pt"` (and similarly for test)
+           `f"{log_dir}/checkpoints/{step}/reward_{train,test}.pt"`
             where step is either the training round or "final".
         - Generator policies are saved to `f"{log_dir}/checkpoints/{step}/gen_policy/"`.
 
