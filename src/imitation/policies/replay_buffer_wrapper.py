c5ada2dac732751757256508985ef672a1de4a69
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2022-07-17 20:55:53 -0700

    code simplification and formatting

--- src/imitation/policies/replay_buffer_wrapper.py
+++ src/imitation/policies/replay_buffer_wrapper.py
@@ -1,23 +1,37 @@
 """Wrapper for reward labeling for transitions sampled from a replay buffer."""
 
 
+from typing import Mapping
+
 import numpy as np
 import torch as th
 from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.type_aliases import ReplayBufferSamples
 
-from imitation.rewards.common import RewardFn
+from imitation.rewards.reward_function import RewardFn
 from imitation.util import util
 
 
-class ReplayBufferRewardWrapper(ReplayBuffer):
+def _samples_to_reward_fn_input(
+    samples: ReplayBufferSamples,
+) -> Mapping[str, np.ndarray]:
+    """Convert a sample from a replay buffer to a numpy array."""
+    return dict(
+        state=samples.observations.cpu().numpy(),
+        action=samples.actions.cpu().numpy(),
+        next_state=samples.next_observations.cpu().numpy(),
+        done=samples.dones.cpu().numpy(),
+    )
+
+
+class ReplayBufferRewardWrapper:
     """Relabel the rewards in transitions sampled from a ReplayBuffer."""
 
     def __init__(self, replay_buffer: ReplayBuffer, reward_fn: RewardFn):
         """Builds ReplayBufferRewardWrapper.
 
         Args:
-            replay_buffer: The wrapped replay buffer.
+            replay_buffer: Replay buffer to wrap.
             reward_fn: The reward function used to relabel rewards.
         """
         self.replay_buffer = replay_buffer
@@ -26,12 +40,7 @@ class ReplayBufferRewardWrapper(ReplayBuffer):
     def sample(self, *args, **kwargs):
         samples = self.replay_buffer.sample(*args, **kwargs)
 
-        rewards = self.reward_fn(
-            samples.observations.cpu().numpy(),
-            samples.actions.cpu().numpy(),
-            samples.next_observations.cpu().numpy(),
-            samples.dones.cpu().numpy(),
-        )
+        rewards = self.reward_fn(**_samples_to_reward_fn_input(samples))
 
         device = samples.rewards.device
         shape = samples.rewards.shape
