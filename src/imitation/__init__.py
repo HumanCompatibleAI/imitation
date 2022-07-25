fbcacd819f9dc13a14fe0e0cdcbb6894e7cd9d71
Adam Gleave <adam@gleave.me>
2022-07-25 11:37:09 -0700

    No cover for clause not expected to hit

--- src/imitation/__init__.py
+++ src/imitation/__init__.py
@@ -4,6 +4,6 @@ from importlib import metadata
 
 try:
     __version__ = metadata.version("imitation")
-except metadata.PackageNotFoundError:
+except metadata.PackageNotFoundError:  # pragma: no cover
     # package is not installed
     pass
