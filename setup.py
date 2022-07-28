7786b912ae2e4f13c469d922133caacb25e9a15d
Lev McKinney <levmckinney@gmail.com>
2022-07-28 16:36:53 -0700

    added python 3.9 and 3.10 to classifiers

--- setup.py
+++ setup.py
@@ -162,6 +162,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
