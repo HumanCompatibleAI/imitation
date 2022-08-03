1774e5ed139d5d475424d8ea30d06c405524889e
Tom Tseng <tom.hm.tseng@gmail.com>
2022-08-03 00:08:22 -0400

    build_and_activate_venv.sh: rm mujoco special case (to be addressed in separate PR)

--- ci/build_and_activate_venv.sh
+++ ci/build_and_activate_venv.sh
@@ -1,56 +1,24 @@
 #!/usr/bin/env bash
+#
+# usage: ./build_and_activate_venv.sh [venv_path] [python_version]
+#   venv_path: Path at which the virtualenv directory should be created. Defaults to
+#     'venv'.
+#   python_version: Version of python to be used in the virtualenv. Defaults to
+#     'python3.8'.
 
 set -e  # exit immediately on any error
-
-program_name=$0
-
-usage() {
-  echo "usage: ${program_name} [--skip-mujoco] [venv_path]";
-  echo "  --skip-mujoco: Skip installing MuJoCo-related packages."
-  echo "  venv_path: Path at which the virtualenv directory should be created. Defaults"
-  echo "    to 'venv'."
-}
-
-# Transform long options to short ones, as getopts doesn't support long options
-# (https://stackoverflow.com/a/30026641/4865149)
-for arg in "$@"; do
-  shift
-  case "$arg" in
-    '--skip-mujoco') set -- "$@" '-m'   ;;
-    *)               set -- "$@" "$arg" ;;
-  esac
-done
-
-skip_mujoco=false
-# Read command line flags
-while getopts ":m" opt; do
-  case $opt in
-    m)
-      skip_mujoco=true
-      echo "--skip-mujoco was triggered!" >&2
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      usage
-      exit 1
-      ;;
-  esac
-done
-# shift all processed options away so that positional arguments can be accessed with
-# $1, $2, etc.
-shift $((OPTIND-1))
 
 venv=$1
 if [[ ${venv} == "" ]]; then
   venv="venv"
 fi
+python_version=$2
+if [[ ${venv} == "" ]]; then
+  python_version="python3.8"
+fi
 
-virtualenv -p python3.9 ${venv}
+virtualenv -p ${python_version} ${venv}
 # shellcheck disable=SC1090,SC1091
 source ${venv}/bin/activate
 python -m pip install --upgrade pip
-if [[ ${skip_mujoco} ]]; then
-  pip install ".[docs,parallel,test]"
-else
-  pip install ".[docs,parallel,test,mujoco]"
-fi
+pip install ".[docs,parallel,test]"
