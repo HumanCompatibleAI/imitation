f4104c5a8865487cc4cf6d7ecdfbef5b620adf58
M. Ernestus <maximilian@ernestus.de>
2022-08-06 01:58:10 +0200

    Pin test and doc requirements (#517)
    * Pin test and doc requirements to known working versions and add reasoning why we did so as a note.
    
    * Don't specify the gym version restriction twice.
    
    * Fix typo in wandb version requirement.
    
    * Fix formatting.
    
    * Pin pytype.
    
    * Add TODO to upgrade jupyter client when a given issue is fixed.
    
    * Try removing code section that lost coverage for absolutely unclear reasons.
    
    * Revert "Try removing code section that lost coverage for absolutely unclear reasons."
    
    This reverts commit 9682d831ece6d37e3dccf2db33adcd5e156142c4.
    
    Co-authored-by: Adam Gleave <adam@gleave.me>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools.command.install import install
 IS_NOT_WINDOWS = os.name != "nt"
 
 PARALLEL_REQUIRE = ["ray[debug,tune]>=1.13.0"]
-PYTYPE = ["pytype"] if IS_NOT_WINDOWS else []
+PYTYPE = ["pytype==2022.7.26"] if IS_NOT_WINDOWS else []
 if IS_NOT_WINDOWS:
     # TODO(adam): use this for Windows as well once PyPI is at >=1.6.1
     STABLE_BASELINES3 = "stable-baselines3>=1.6.0"
@@ -20,48 +20,53 @@ else:
         "https://github.com/DLR-RM/stable-baselines3.git@master"
     )
 
+# pinned to 0.21 until https://github.com/DLR-RM/stable-baselines3/pull/780 goes
+# upstream.
+GYM_VERSION_SPECIFIER = "==0.21.0"
+
+# Note: the versions of the test and doc requirements should be tightly pinned to known
+#   working versions to make our CI/CD pipeline as stable as possible.
 TESTS_REQUIRE = (
     [
-        "seals",
-        "black[jupyter]",
-        "coverage",
-        "codecov",
-        "codespell",
-        "darglint",
-        "filelock",
-        # TODO(adam): remove pin once flake8-isort fixed:
-        #  https://github.com/gforcada/flake8-isort/issues/115
+        "seals==0.1.2",
+        "black[jupyter]~=22.6.0",
+        "coverage~=6.4.2",
+        "codecov~=2.1.12",
+        "codespell~=2.1.0",
+        "darglint~=1.8.1",
+        "filelock~=3.7.1",
         "flake8~=4.0.1",
-        "flake8-blind-except",
-        "flake8-builtins",
-        "flake8-commas",
-        "flake8-debugger",
-        "flake8-docstrings",
-        "flake8-isort",
-        "hypothesis",
-        "ipykernel",
-        "jupyter",
-        # remove pin once https://github.com/jupyter/jupyter_client/issues/637 fixed
-        "jupyter-client<7.0",
-        "pandas",
-        "pytest",
-        "pytest-cov",
-        "pytest-notebook",
-        "pytest-xdist",
-        "scipy>=1.8.0",
-        "wandb",
+        "flake8-blind-except==0.2.1",
+        "flake8-builtins~=1.5.3",
+        "flake8-commas~=2.1.0",
+        "flake8-debugger~=4.1.2",
+        "flake8-docstrings~=1.6.0",
+        "flake8-isort~=4.1.2",
+        "hypothesis~=6.54.1",
+        "ipykernel~=6.15.1",
+        "jupyter~=1.0.0",
+        # TODO: upgrade jupyter-client once
+        #  https://github.com/jupyter/jupyter_client/issues/637 is fixed
+        "jupyter-client~=6.1.12",
+        "pandas~=1.4.3",
+        "pytest~=7.1.2",
+        "pytest-cov~=3.0.0",
+        "pytest-notebook==0.8.0",
+        "pytest-xdist~=2.5.0",
+        "scipy~=1.9.0",
+        "wandb==0.12.21",
     ]
     + PARALLEL_REQUIRE
     + PYTYPE
 )
 DOCS_REQUIRE = [
     "sphinx~=5.1.1",
-    "sphinx-autodoc-typehints",
-    "sphinx-rtd-theme",
-    "sphinxcontrib-napoleon",
-    "furo",
-    "sphinx-copybutton",
-    "sphinx-github-changelog",
+    "sphinx-autodoc-typehints~=1.19.1",
+    "sphinx-rtd-theme~=1.0.0",
+    "sphinxcontrib-napoleon==0.7",
+    "furo==2022.6.21",
+    "sphinx-copybutton==0.5.0",
+    "sphinx-github-changelog~=1.2.0",
 ]
 
 
@@ -103,11 +108,12 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"imitation": ["py.typed", "envs/examples/airl_envs/assets/*.xml"]},
+    # Note: while we are strict with our test and doc requirement versions, we try to
+    #   impose as little restrictions on the install requirements as possible. Try to
+    #   encode only known incompatibilities here. This prevents nasty dependency issues
+    #   for our users.
     install_requires=[
-        # If you change gym version here, change it in "mujoco" below too.
-        # pinned to 0.21 until https://github.com/DLR-RM/stable-baselines3/pull/780
-        # goes upstream.
-        "gym[classic_control]==0.21.0",
+        "gym[classic_control]" + GYM_VERSION_SPECIFIER,
         "matplotlib",
         "numpy>=1.15",
         "torch>=1.4.0",
@@ -139,7 +145,7 @@ setup(
         "docs": DOCS_REQUIRE,
         "parallel": PARALLEL_REQUIRE,
         "mujoco": [
-            "gym[classic_control,mujoco]==0.21.0",
+            "gym[classic_control,mujoco]" + GYM_VERSION_SPECIFIER,
         ],
     },
     entry_points={
