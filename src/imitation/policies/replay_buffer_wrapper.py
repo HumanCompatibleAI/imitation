d917b49450927a86dfa6d8d03a5da4c6fdc93600
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2022-08-08 22:28:28 -0700

    remove size() reset() and to_torch() that operate with pos, full, and device

--- src/imitation/policies/replay_buffer_wrapper.py
+++ src/imitation/policies/replay_buffer_wrapper.py
@@ -4,7 +4,6 @@
 from typing import Mapping, Type
 
 import numpy as np
-import torch as th
 from gym import spaces
 from stable_baselines3.common.buffers import BaseBuffer, ReplayBuffer
 from stable_baselines3.common.type_aliases import ReplayBufferSamples
@@ -98,14 +97,8 @@ class ReplayBufferRewardWrapper(BaseBuffer):
     def add(self, *args, **kwargs):
         self.replay_buffer.add(*args, **kwargs)
 
-    def size(self) -> int:
-        return self.replay_buffer.size()
-
-    def reset(self) -> None:
-        self.replay_buffer.reset()
-
-    def to_torch(self, array: np.ndarray, copy: bool = True) -> th.Tensor:
-        return self.replay_buffer.to_torch(array, copy)
-
     def _get_samples(self):
-        raise NotImplementedError("_get_samples is intentionally not implemented.")
+        raise NotImplementedError(
+            "_get_samples() is intentionally not implemented."
+            "This method should not be called.",
+        )
