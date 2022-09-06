3c1aaa602ab42da0464ffe67f45896dfb9be8ad0
taufeeque9 <9taufeeque9@gmail.com>
2022-09-06 05:59:27 +0530

    Add wandb and print_config to train_experts

--- experiments/train_experts.sh
+++ experiments/train_experts.sh
@@ -13,6 +13,7 @@ ENVS=(seals_ant seals_half_cheetah)
 OUTPUT_DIR="output/train_experts/${TIMESTAMP}"
 RESULTS_FILE="results.txt"
 extra_configs=()
+print_config=0
 
 if ! TEMP=$($GNU_GETOPT -o frw -l fast,regenerate,wandb -- "$@"); then
   exit 1
@@ -30,7 +31,7 @@ while true; do
       ;;
     -w | --wandb)
       # activate wandb logging by adding 'wandb' format string to common.log_format_strs
-      extra_configs=("${extra_configs[@]}" "common.wandb_logging")
+      extra_configs=("${extra_configs[@]}" "common.wandb_logging" "common.wandb.wandb_kwargs.project='algorithm-benchmark' ")
       shift
       ;;
     -r | --regenerate)
@@ -50,6 +51,10 @@ while true; do
 
       shift
       ;;
+    -p | --print)
+      print_config=1
+      shift
+      ;;
     --)
       shift
       break
@@ -60,6 +65,21 @@ while true; do
       ;;
   esac
 done
+
+if [[ $print_config ]]; then
+  parallel -j 25% --header : --progress --results ${OUTPUT_DIR}/parallel/ \
+    python -m imitation.scripts.train_rl print_config \
+    --capture=sys \
+    with \
+    '{env}' "${extra_configs[@]}" \
+    seed='{seed}' \
+    common.log_dir="${OUTPUT_DIR}/{env}_{seed}" \
+    ::: env "${ENVS[@]}" \
+    ::: seed "${SEEDS[@]}"
+    exit 0
+fi
+
+
 
 echo "Writing logs in ${OUTPUT_DIR}"
 # Train experts.
