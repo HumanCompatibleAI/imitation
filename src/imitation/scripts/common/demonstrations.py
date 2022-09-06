497d4f05594ab9380d3f5460d86b736c15a79d53
Maximilian Ernestus <maximilian@ernestus.de>
2022-09-06 10:12:04 +0200

    Remove now unneeded magic number.

--- src/imitation/scripts/common/demonstrations.py
+++ src/imitation/scripts/common/demonstrations.py
@@ -70,7 +70,7 @@ def generate_expert_trajs(
     return rollout.rollout(
         expert.get_expert_policy(rollout_env),
         rollout_env,
-        rollout.make_sample_until(min_timesteps=2000, min_episodes=n_expert_demos),
+        rollout.make_sample_until(min_episodes=n_expert_demos),
     )
 
 
