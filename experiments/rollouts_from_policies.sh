8aaabe792c99e9d470fc74726bdd0d0d9a117887
taufeeque9 <9taufeeque9@gmail.com>
2022-09-08 05:28:47 +0530

    Modify rollout scripts

--- experiments/rollouts_from_policies.sh
+++ experiments/rollouts_from_policies.sh
@@ -16,9 +16,8 @@ set -e  # Exit on error.
 source experiments/common.sh
 
 CONFIG_CSV=${CONFIG_CSV:-experiments/rollouts_from_policies_config.csv}
-OUTPUT_DIR="output/train_experts/${TIMESTAMP}"
 
-if ! TEMP=$($GNU_GETOPT -o f -l fast -- "$@"); then
+if ! TEMP=$($GNU_GETOPT -o f -l fast,timestamp: -- "$@"); then
   exit 1
 fi
 eval set -- "$TEMP"
@@ -29,6 +28,10 @@ while true; do
       # Fast mode (debug)
       CONFIG_CSV="tests/testdata/rollouts_from_policies_config.csv"
       shift
+      ;;
+    --timestamp)
+      TIMESTAMP="$2"
+      shift 2
       ;;
     --)
       shift
@@ -41,15 +44,17 @@ while true; do
   esac
 done
 
+OUTPUT_DIR="output/train_experts/${TIMESTAMP}"
+
 echo "Writing logs in ${OUTPUT_DIR}, and saving rollouts in ${OUTPUT_DIR}/expert_models/*/rollouts/"
 
-parallel -j 25% --header : --results "${OUTPUT_DIR}/parallel/" --colsep , \
-  python -m imitation.scripts.eval_policy \
+parallel -j 25% --header : --results "${OUTPUT_DIR}/parallel_rollout/" --progress --colsep , \
+  python3 -m imitation.scripts.eval_policy \
   --capture=sys \
   with \
   '{env_config_name}' \
   common.log_root="${OUTPUT_DIR}" \
-  rollout_save_path="${OUTPUT_DIR}/{env_config_name}_0/rollouts/final.pkl" \
+  rollout_save_path="${OUTPUT_DIR}/{env_config_name}_{best_seed}/rollouts/final.pkl" \
   eval_n_episodes='{n_demonstrations}' \
   eval_n_timesteps=None \
   :::: ${CONFIG_CSV}
