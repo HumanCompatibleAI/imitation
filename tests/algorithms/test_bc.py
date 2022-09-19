59e386834b6fd0e18f39ad28d210514c3c6e3f1f
Maximilian Ernestus <maximilian@ernestus.de>
2022-09-19 17:34:19 +0200

    Introduce hypothesis testing to BC tests and improve test naming.

--- tests/algorithms/test_bc.py
+++ tests/algorithms/test_bc.py
@@ -3,77 +3,197 @@
 import dataclasses
 import os
 
+import gym
+import hypothesis
+import hypothesis.strategies as st
 import pytest
 import torch as th
 from stable_baselines3.common import evaluation, vec_env
 from torch.utils import data as th_data
 
+from conftest import get_expert_trajectories
 from imitation.algorithms import bc
 from imitation.data import rollout, types
+from imitation.data.wrappers import RolloutInfoWrapper
 from imitation.testing import reward_improvement
 from imitation.util import logger, util
 
 
-@pytest.fixture(params=[32, 50])
-def batch_size(request):
-    return request.param
-
-
-@pytest.fixture(params=["data_loader", "ducktyped_data_loader", "transitions"])
-def expert_data_type(request):
-    return request.param
-
-
-class DucktypedDataset:
-    """Used to check that any iterator over Dict[str, Tensor] works with BC."""
-
-    def __init__(self, transitions: types.TransitionsMinimal, batch_size: int):
-        """Builds `DucktypedDataset`."""
-        self.trans = transitions
-        self.batch_size = batch_size
-
-    def __iter__(self):
-        for start in range(0, len(self.trans) - self.batch_size, self.batch_size):
-            end = start + self.batch_size
-            d = dict(obs=self.trans.obs[start:end], acts=self.trans.acts[start:end])
-            d = {k: util.safe_to_tensor(v) for k, v in d.items()}
-            yield d
-
-
-@pytest.fixture
-def trainer(
+def make_expert_trajectory_loader(
+    pytestconfig,
     batch_size,
-    cartpole_venv,
     expert_data_type,
-    custom_logger,
-    cartpole_expert_trajectories,
+    env_name: str,
+    max_num_trajectories: int = -1,
 ):
-    trans = rollout.flatten_trajectories(cartpole_expert_trajectories)
+    trajectories = get_expert_trajectories(pytestconfig, env_name)
+    transitions = rollout.flatten_trajectories(
+        trajectories[: max(max_num_trajectories, batch_size)]
+    )
     if expert_data_type == "data_loader":
-        expert_data = th_data.DataLoader(
-            trans,
+        return th_data.DataLoader(
+            transitions,
             batch_size=batch_size,
             shuffle=True,
             drop_last=True,
             collate_fn=types.transitions_collate_fn,
         )
     elif expert_data_type == "ducktyped_data_loader":
-        expert_data = DucktypedDataset(trans, batch_size)
+
+        class DucktypedDataset:
+            """Used to check that any iterator over Dict[str, Tensor] works with BC."""
+
+            def __init__(self, transitions: types.TransitionsMinimal, batch_size: int):
+                """Builds `DucktypedDataset`."""
+                self.trans = transitions
+                self.batch_size = batch_size
+
+            def __iter__(self):
+                for start in range(
+                    0, len(self.trans) - self.batch_size, self.batch_size
+                ):
+                    end = start + self.batch_size
+                    d = dict(
+                        obs=self.trans.obs[start:end], acts=self.trans.acts[start:end]
+                    )
+                    d = {k: util.safe_to_tensor(v) for k, v in d.items()}
+                    yield d
+
+        return DucktypedDataset(transitions, batch_size)
     elif expert_data_type == "transitions":
-        expert_data = trans
+        return transitions
     else:  # pragma: no cover
         raise ValueError(expert_data_type)
 
+
+@pytest.fixture
+def cartpole_bc_trainer(
+    pytestconfig,
+    cartpole_venv,
+    cartpole_expert_trajectories,
+):
     return bc.BC(
         observation_space=cartpole_venv.observation_space,
         action_space=cartpole_venv.action_space,
+        batch_size=50,
+        demonstrations=make_expert_trajectory_loader(
+            pytestconfig,
+            50,
+            "transitions",
+            "seals/CartPole-v0",
+        ),
+        custom_logger=None,
+    )
+
+
+# Note: we don't use the Mujoco envs here because mujoco is not installed on CI.
+envs = st.sampled_from(["Pendulum-v1", "seals/CartPole-v0"])
+batch_sizes = st.integers(min_value=1, max_value=50)
+env_numbers = st.integers(min_value=1, max_value=10)
+loggers = st.sampled_from([None, logger.configure()])
+expert_data_types = st.sampled_from(
+    ["data_loader", "ducktyped_data_loader", "transitions"],
+)
+
+
+@st.composite
+def bc_train_args(draw):
+    args = dict(
+        on_epoch_end=draw(st.sampled_from([None, lambda: None])),
+        on_batch_end=draw(st.sampled_from([None, lambda: None])),
+        log_interval=draw(st.integers(500, 10000)),
+        log_rollouts_n_episodes=draw(st.sampled_from([-1, 2])),
+        progress_bar=draw(st.booleans()),
+        reset_tensorboard=draw(st.booleans()),
+    )
+    duration_measure = draw(st.sampled_from(["n_batches", "n_epochs"]))
+    duration = draw(st.integers(1, 3))
+    args[duration_measure] = duration
+    return args
+
+
+@hypothesis.given(
+    batch_size=batch_sizes,
+    env_name=envs,
+    num_envs=env_numbers,
+    custom_logger=loggers,
+    expert_data_type=expert_data_types,
+)
+# Setting the deadline to none since during the first runs, the expert trajectories must
+# be computed. Later they can be loaded from cache much faster.
+@hypothesis.settings(deadline=None)
+def test_smoke_bc_creation(
+    batch_size,
+    env_name,
+    num_envs,
+    custom_logger,
+    expert_data_type,
+    pytestconfig,
+):
+    # GIVEN
+    env = vec_env.DummyVecEnv(
+        [lambda: RolloutInfoWrapper(gym.make(env_name)) for _ in range(num_envs)],
+    )
+    bc.BC(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
         batch_size=batch_size,
-        demonstrations=expert_data,
+        demonstrations=make_expert_trajectory_loader(
+            pytestconfig,
+            batch_size,
+            expert_data_type,
+            env_name,
+        ),
         custom_logger=custom_logger,
     )
 
 
-def test_weight_decay_init_error(cartpole_venv, custom_logger):
+@hypothesis.given(
+    env_name=envs,
+    num_envs=env_numbers,
+    train_args=bc_train_args(),
+    use_custom_rollout_venv=st.booleans(),
+    batch_size=batch_sizes,
+    expert_data_type=expert_data_types,
+)
+@hypothesis.settings(deadline=10000, max_examples=50)
+def test_smoke_bc_training(
+    env_name,
+    num_envs,
+    train_args,
+    use_custom_rollout_venv,
+    batch_size,
+    expert_data_type,
+    pytestconfig,
+):
+    # GIVEN
+    env = util.make_vec_env(env_name, num_envs)
+    trainer = bc.BC(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+        batch_size=batch_size,
+        demonstrations=make_expert_trajectory_loader(
+            pytestconfig,
+            batch_size,
+            expert_data_type,
+            env_name,
+            max_num_trajectories=3,  # Only use 3 trajectories to speed up the test
+        ),
+    )
+    if use_custom_rollout_venv:
+        custom_rollout_venv = util.make_vec_env(
+            env_name,
+            num_envs,
+            post_wrappers=[lambda e, _: RolloutInfoWrapper(e)]
+        )
+    else:
+        custom_rollout_venv = None
+
+    # WHEN
+    trainer.train(log_rollouts_venv=custom_rollout_venv, **train_args)
+
+
+def test_that_weight_decay_in_optimizer_raises_error(cartpole_venv, custom_logger):
     with pytest.raises(ValueError, match=".*weight_decay.*"):
         bc.BC(
             observation_space=cartpole_venv.observation_space,
@@ -84,36 +204,38 @@ def test_weight_decay_init_error(cartpole_venv, custom_logger):
         )
 
 
-def test_train_end_cond_error(trainer: bc.BC):
-    err_context = pytest.raises(ValueError, match="exactly one.*n_epochs")
-    with err_context:
-        trainer.train(n_epochs=1, n_batches=10)
-    with err_context:
-        trainer.train()
-    with err_context:
-        trainer.train(n_epochs=None, n_batches=None)
+@pytest.mark.parametrize(duration_args = [
+    dict(n_epochs=1, n_batches=10),
+    dict(),
+    dict(n_epochs=None, n_batches=None),
+])
+def test_that_wrong_training_duration_specification_raises_error(
+        cartpole_bc_trainer,
+        duration_args,
+):
+    with pytest.raises(ValueError, match="exactly one.*n_epochs"):
+        cartpole_bc_trainer.train(**duration_args)
 
 
-def test_bc(trainer: bc.BC, cartpole_venv):
+def test_that_bc_improves_rewards(cartpole_bc_trainer, cartpole_venv):
+    # GIVEN
     novice_rewards, _ = evaluation.evaluate_policy(
-        trainer.policy,
+        cartpole_bc_trainer.policy,
         cartpole_venv,
         15,
         return_episode_rewards=True,
     )
 
-    trainer.train(
-        n_epochs=1,
-        on_epoch_end=lambda: print("epoch end"),
-        on_batch_end=lambda: print("batch end"),
-    )
-    trainer.train(n_batches=10)
+    # WHEN
+    cartpole_bc_trainer.train(n_epochs=1)
     rewards_after_training, _ = evaluation.evaluate_policy(
-        trainer.policy,
+        cartpole_bc_trainer.policy,
         cartpole_venv,
         15,
         return_episode_rewards=True,
     )
+
+    # THEN
     assert reward_improvement.is_significant_reward_improvement(
         novice_rewards,
         rewards_after_training,
@@ -125,8 +247,8 @@ def test_bc(trainer: bc.BC, cartpole_venv):
     )
 
 
-def test_bc_log_rollouts(trainer: bc.BC, cartpole_venv):
-    trainer.train(
+def test_smoke_log_rollouts(cartpole_bc_trainer, cartpole_venv):
+    cartpole_bc_trainer.train(
         n_batches=20,
         log_rollouts_venv=cartpole_venv,
         log_rollouts_n_episodes=1,
@@ -158,7 +280,7 @@ class _DataLoaderFailsOnNthIter:
 
 
 @pytest.mark.parametrize("no_yield_after_iter", [0, 1, 5])
-def test_bc_data_loader_empty_iter_error(
+def test_that_bc_raises_error_when_data_loader_is_empty(
     cartpole_venv: vec_env.VecEnv,
     no_yield_after_iter: bool,
     custom_logger: logger.HierarchicalLogger,
@@ -193,10 +315,10 @@ def test_bc_data_loader_empty_iter_error(
         trainer.train(n_batches=20)
 
 
-def test_save_reload(trainer, tmpdir):
+def test_save_reload(cartpole_bc_trainer, tmpdir):
     pol_path = os.path.join(tmpdir, "policy.pt")
-    var_values = list(trainer.policy.parameters())
-    trainer.save_policy(pol_path)
+    var_values = list(cartpole_bc_trainer.policy.parameters())
+    cartpole_bc_trainer.save_policy(pol_path)
     new_policy = bc.reconstruct_policy(pol_path)
     new_values = list(new_policy.parameters())
     assert len(var_values) == len(new_values)
@@ -204,13 +326,13 @@ def test_save_reload(trainer, tmpdir):
         assert th.allclose(old, new)
 
 
-def test_train_progress_bar_visibility(trainer: bc.BC):
+def test_train_progress_bar_visibility(cartpole_bc_trainer):
     """Smoke test for toggling progress bar visibility."""
     for visible in [True, False]:
-        trainer.train(n_batches=1, progress_bar=visible)
+        cartpole_bc_trainer.train(n_batches=1, progress_bar=visible)
 
 
-def test_train_reset_tensorboard(trainer: bc.BC):
+def test_train_reset_tensorboard(cartpole_bc_trainer):
     """Smoke test for reset_tensorboard parameter."""
     for reset in [True, False]:
-        trainer.train(n_batches=1, reset_tensorboard=reset)
+        cartpole_bc_trainer.train(n_batches=1, reset_tensorboard=reset)
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,42 +7,52 @@ from typing import Sequence
 
 import gym
 import pytest
+import seals  # noqa: F401
 import torch
 from filelock import FileLock
-from huggingface_sb3 import load_from_hub
-from stable_baselines3 import PPO
 from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv
 
-from imitation.data import rollout, types
+import huggingface_sb3 as hfsb3
+from imitation.data import rollout, types, wrappers
 from imitation.data.types import TrajectoryWithRew
 from imitation.data.wrappers import RolloutInfoWrapper
-from imitation.util import logger
+from imitation.policies import serialize
+from imitation.util import logger, util
 
-CARTPOLE_ENV_NAME = "CartPole-v1"
+CARTPOLE_ENV_NAME = "seals/CartPole-v0"
 
 
-def load_or_rollout_trajectories(
-    cache_path,
-    policy,
-    venv,
-) -> Sequence[TrajectoryWithRew]:
-    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
-    with FileLock(cache_path + ".lock"):
+def get_expert_trajectories(pytestconfig, env_name: str, min_episodes: int = 60):
+    trajectories_path = str(
+        pytestconfig.cache.makedir("experts")
+        / hfsb3.EnvironmentName(env_name)
+        / "rollout.npz",
+    )
+
+    os.makedirs(os.path.dirname(trajectories_path), exist_ok=True)
+    with FileLock(trajectories_path + ".lock"):
         try:
-            return types.load_with_rewards(cache_path)
-        except (OSError, pickle.PickleError):  # pragma: no cover
+            trajectories = types.load_with_rewards(trajectories_path)
+        except (FileNotFoundError, pickle.PickleError):  # pragma: no cover
             warnings.warn(
                 "Recomputing expert trajectories due to the following error when "
                 "trying to load them:\n" + traceback.format_exc(),
             )
-            rollouts = rollout.rollout(
-                policy,
-                venv,
-                rollout.make_sample_until(min_timesteps=2000, min_episodes=57),
+
+            env = util.make_vec_env(
+                env_name,
+                log_dir=None,
+                post_wrappers=[lambda env, i: wrappers.RolloutInfoWrapper(env)],
             )
-            types.save(cache_path, rollouts)
-            return rollouts
+            trajectories = rollout.rollout(
+                serialize.load_policy("ppo-huggingface", env, env_name=env_name),
+                env,
+                rollout.make_sample_until(min_episodes=min_episodes),
+            )
+            env.close()
+            types.save(trajectories_path, trajectories)
+        return trajectories
 
 
 @pytest.fixture(params=[1, 4])
@@ -57,13 +67,12 @@ def cartpole_venv(request) -> VecEnv:
 
 
 @pytest.fixture
-def cartpole_expert_policy():
-    return PPO.load(
-        load_from_hub(
-            "HumanCompatibleAI/ppo-seals-CartPole-v0",
-            "ppo-seals-CartPole-v0.zip",
-        ),
-    ).policy
+def cartpole_expert_policy(cartpole_venv: VecEnv) -> BasePolicy:
+    return serialize.load_policy(
+        "ppo-huggingface",
+        cartpole_venv,
+        env_name=CARTPOLE_ENV_NAME,
+    )
 
 
 @pytest.fixture
@@ -72,14 +81,7 @@ def cartpole_expert_trajectories(
     cartpole_venv,
     pytestconfig,
 ) -> Sequence[TrajectoryWithRew]:
-    rollouts_path = str(
-        pytestconfig.cache.makedir("experts") / CARTPOLE_ENV_NAME / "rollout.npz",
-    )
-    return load_or_rollout_trajectories(
-        rollouts_path,
-        cartpole_expert_policy,
-        cartpole_venv,
-    )
+    return get_expert_trajectories(pytestconfig, CARTPOLE_ENV_NAME)
 
 
 PENDULUM_ENV_NAME = "Pendulum-v1"
@@ -91,29 +93,17 @@ def pendulum_venv() -> VecEnv:
 
 
 @pytest.fixture
-def pendulum_expert_policy() -> BasePolicy:
-    return PPO.load(
-        load_from_hub(
-            "HumanCompatibleAI/ppo-Pendulum-v1",
-            "ppo-Pendulum-v1.zip",
-        ),
-    ).policy
+def pendulum_expert_policy(pendulum_venv) -> BasePolicy:
+    return serialize.load_policy(
+        "ppo-huggingface", pendulum_venv, env_name=PENDULUM_ENV_NAME
+    )
 
 
 @pytest.fixture
 def pendulum_expert_trajectories(
-    pendulum_expert_policy,
-    pendulum_venv,
     pytestconfig,
 ) -> Sequence[TrajectoryWithRew]:
-    rollouts_path = str(
-        pytestconfig.cache.makedir("experts") / PENDULUM_ENV_NAME / "rollout.npz",
-    )
-    return load_or_rollout_trajectories(
-        rollouts_path,
-        pendulum_expert_policy,
-        pendulum_venv,
-    )
+    return get_expert_trajectories(pytestconfig, PENDULUM_ENV_NAME)
 
 
 @pytest.fixture(scope="session", autouse=True)
