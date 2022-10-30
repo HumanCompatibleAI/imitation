9c5f0b0dd33d878deaf9ee1d99d1a4ea09d969cc
Adam Gleave <adam@gleave.me>
2022-10-29 18:10:12 -0700

    Add assert to self-document minibatch size is fixed

--- src/imitation/algorithms/adversarial/common.py
+++ src/imitation/algorithms/adversarial/common.py
@@ -356,8 +356,9 @@ class AdversarialTrainer(base.DemonstrationAlgorithm[types.Transitions]):
                     batch["labels_expert_is_one"].float(),
                 )
 
-                # Renormalise the loss to be averaged over the whole
-                # batch size instead of the minibatch size.
+                # Renormalise the loss to be averaged over the whole batch size
+                # instead of the minibatch size.
+                assert len(batch["state"]) == self.demo_minibatch_size
                 loss *= self.demo_minibatch_size / self.demo_batch_size
                 loss.backward()
 
