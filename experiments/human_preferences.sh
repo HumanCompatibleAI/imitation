ae0087ffd99f1e0e3e6626727407361d35442a4e
rk1a <robertklassert@pm.me>
2022-12-30 12:37:44 +0100

    Fix PrefCollect address

--- experiments/human_preferences.sh
+++ experiments/human_preferences.sh
@@ -4,7 +4,7 @@ python -m imitation.scripts.train_preference_comparisons \
   human_preferences \
   total_comparisons=5000 \
   total_timesteps=1000000 \
-  gatherer_kwargs.pref_collect_address=127.0.0.1:8000 \
+  gatherer_kwargs.pref_collect_address=http://127.0.0.1:8000 \
   gatherer_kwargs.video_output_dir=../pref-collect/videofiles \
   gatherer_kwargs.wait_for_user=True \
   common.post_wrappers_kwargs.RenderImageInfoWrapper.scale_factor=0.5 \
