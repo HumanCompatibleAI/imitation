32ebd2b76772842ee67b296a970b251b181402f8
Maximilian Ernestus <maximilian@ernestus.de>
2023-02-20 13:38:11 +0100

    Fix convert_trajs.py script and its tests.

--- src/imitation/scripts/convert_trajs.py
+++ src/imitation/scripts/convert_trajs.py
@@ -1,42 +1,53 @@
-"""Converts old-style pickle trajectories to new-style NPZ trajectories.
+"""Converts old-style pickle or npz trajectories to new-style HuggingFace datasets.
 
 See https://github.com/HumanCompatibleAI/imitation/pull/448 for a description
 of the new trajectory format.
 
 This script takes as command-line input multiple paths to saved trajectories,
-in the old .pkl or new .npz format. It then saves each sequence in the new .npz
-format. The path is the same as the original but with an ".npz" extension
-(i.e. "A.pkl" -> "A.npz", "A.npz" -> "A.npz", "A" -> "A.npz", "A.foo" -> "A.foo.npz").
+in the old .pkl or .npz format. It then saves each sequence in the new HuggingFace
+datasets format. The path is the same as the original but a directory without an
+extension (i.e. "A.pkl" -> "A/", "A.npz" -> "A/", "A/" -> "A/", "A.foo" -> "A/").
 """
-
+import pathlib
 import warnings
 
 import imitation.data.serialize
+from imitation.data import types
 
 
-def update_traj_file_in_place(path_str: str, /) -> None:
-    """Modifies trajectories pickle file in-place to update data to new format.
+def update_traj_file_in_place(path_str: types.AnyPath, /) -> pathlib.Path:
+    """Converts pickle or npz file to the new HuggingFace format.
 
     The new data is saved as `Sequence[imitation.types.TrajectoryWithRew]`.
 
     Args:
-        path_str: Path to a pickle file containing
+        path_str: Path to a pickle or npz file containing
             `Sequence[imitation.types.Trajectory]`
             or `Sequence[imitation.old_types.TrajectoryWithRew]`.
+
+    Returns:
+        The path to the converted trajectory dataset.
     """
     path = imitation.data.serialize.parse_path(path_str)
     with warnings.catch_warnings():
         # Filter out DeprecationWarning because we expect to load old trajectories here.
         warnings.filterwarnings(
             "ignore",
-            message="Loading old version of Trajectory.*",
+            message="Loading old .* version of Trajectories.*",
             category=DeprecationWarning,
         )
         trajs = imitation.data.serialize.load(path)
 
-    ext = path.suffix
-    new_ext = ".npz" if ext in (".pkl", ".npz") else ext + ".npz"
-    imitation.data.serialize.save(path.with_suffix(new_ext), trajs)
+    if isinstance(
+        trajs,
+        imitation.data.huggingface_datasets_conversion.TrajectoryDatasetSequence,
+    ):
+        warnings.warn(f"File {path} is already in the new format. Skipping.")
+        return path
+    else:
+        converted_path = path.with_suffix("")
+        imitation.data.serialize.save(converted_path, trajs)
+        return converted_path
 
 
 def main():
