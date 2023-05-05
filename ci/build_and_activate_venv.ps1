32182ee466d1384f9bed109f9534b2ebef70fbe2
Adam Gleave <adam@gleave.me>
2023-05-05 14:14:22 -0400

    Pin version of setuptools and wheel

--- ci/build_and_activate_venv.ps1
+++ ci/build_and_activate_venv.ps1
@@ -9,6 +9,7 @@ If ($venv -eq $null) {
 
 virtualenv -p python3.8 $venv
 & $venv\Scripts\activate
-# Note: We need to install setuptools==66.1.1 to allow installing gym==0.21.0.
-python -m pip install --upgrade pip setuptools==66.1.1
+# Note: We need to install these versions of setuptools and wheel to allow installing gym==0.21.0 on Windows.
+# See https://github.com/freqtrade/freqtrade/issues/8376
+python -m pip install --upgrade pip wheel==0.38.4 setuptools==65.5.1
 pip install ".[docs,parallel,test]"
