0db0ed2afff08284739799fb8e5ef85a89216c19
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-08-04 14:35:14 +0200

    Download expert policies from HF hub by default in quickstart.py

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -5,6 +5,7 @@ Refer to the jupyter notebooks for more detailed examples of how to use the algo
 
 import gym
 import numpy as np
+import seals  # needed to load "seals/" environments    # noqa: F401
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv
@@ -13,12 +14,14 @@ from stable_baselines3.ppo import MlpPolicy
 from imitation.algorithms import bc
 from imitation.data import rollout
 from imitation.data.wrappers import RolloutInfoWrapper
+from imitation.policies.serialize import load_policy
 
-env = gym.make("CartPole-v1")
+env = gym.make("seals/CartPole-v0")
 rng = np.random.default_rng(0)
 
 
 def train_expert():
+    # note: use `download_expert` instead to download a pretrained, competent expert
     print("Training a expert.")
     expert = PPO(
         policy=MlpPolicy,
@@ -34,8 +37,20 @@ def train_expert():
     return expert
 
 
+def download_expert():
+    print("Downloading a pretrained expert.")
+    expert = load_policy(
+        "ppo-huggingface",
+        organization="HumanCompatibleAI",
+        env_name="seals-CartPole-v0",
+        venv=env,
+    )
+    return expert
+
+
 def sample_expert_transitions():
-    expert = train_expert()
+    # expert = train_expert()  # uncomment to train your own expert
+    expert = download_expert()
 
     print("Sampling expert transitions.")
     rollouts = rollout.rollout(
@@ -59,7 +74,7 @@ reward, _ = evaluate_policy(
     bc_trainer.policy,  # type: ignore[arg-type]
     env,
     n_eval_episodes=3,
-    render=True,
+    render=True,  # comment out to speed up
 )
 print(f"Reward before training: {reward}")
 
@@ -70,6 +85,6 @@ reward, _ = evaluate_policy(
     bc_trainer.policy,  # type: ignore[arg-type]
     env,
     n_eval_episodes=3,
-    render=True,
+    render=True,  # comment out to speed up
 )
 print(f"Reward after training: {reward}")
