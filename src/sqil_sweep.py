6e1f328bf6a544d747a3e4f17ffdae53aab0e788
lukasberglund <lukastberglund@gmail.com>
2023-09-15 16:06:40 +0200

    save

--- src/sqil_sweep.py
+++ src/sqil_sweep.py
@@ -44,11 +44,11 @@ def objective_cartpole(trial, environment):
 def objective_cheetah(trial):
 
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True) # too broad?
-    batch_size = trial.suggest_categorical("batch_size", [32, 64, 128, 256, 512]) # might be too broad
+    batch_size = trial.suggest_int("batch_size", 10, 10_000) # might be too broad
     gradient_steps = trial.suggest_int("gradient_steps", 1, 20)
     tau = trial.suggest_float("tau", 0.001, 0.5, log=True)
     gamma = trial.suggest_float("gamma", 0.5, 0.999)
-    total_timesteps = trial.suggest_int("total_timesteps", 1000, 1_000_000, log=True)
+    total_timesteps = trial.suggest_int("total_timesteps", 10_000, 100_000, log=True)
 
     config_updates = {
         "demonstrations.n_expert_demos": 50,
@@ -80,24 +80,62 @@ def objective_cheetah(trial):
 
     return get_sqil_results(config_updates, named_configs)    
 
+def perform_cheetah_trial():
+    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
+    study_name = "cheetah_trial_short_max_length2"  # Unique identifier of the study.
+    storage_name = "sqlite:///{}.db".format(study_name)
+    study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True, direction='maximize')
 
-def perform_trial(environment):
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100, n_jobs=20)
+    study.optimize(objective_cheetah, n_trials=20, n_jobs=1)
 
     print(f"Best params: {study.best_params}")
     print(f"Best value: {study.best_value}")
 
-def perform_cheetah_trial():
+
+def objective_ant(trial):
+
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True) # too broad?
+    batch_size = trial.suggest_int("batch_size", 10, 10_000) # might be too broad
+    gradient_steps = trial.suggest_int("gradient_steps", 1, 20)
+    tau = trial.suggest_float("tau", 0.001, 0.5, log=True)
+    gamma = trial.suggest_float("gamma", 0.5, 0.999)
+    total_timesteps = trial.suggest_int("total_timesteps", 10_000, 200_000, log=True)
+    # if things don't work maybe changing entropy coeff will help
+
+    config_updates = {
+        "demonstrations.n_expert_demos": 50,
+        "sqil.total_timesteps": total_timesteps, 
+        # "policy.policy_cls": SACPolicies.SACPolicy,
+
+        "sqil.train_kwargs": {
+            "progress_bar": True,
+        },
+        "rl.rl_kwargs": {
+            "learning_rate": learning_rate,
+            "batch_size": batch_size,
+            "gradient_steps": gradient_steps,
+            "tau": tau,
+            "gamma": gamma,
+            "learning_starts": 1000,
+        },
+    }
+
+    named_configs = ["rl.sac", "policy.sac256", "seals_ant"]
+
+
+    return get_sqil_results(config_updates, named_configs)    
+
+
+def perform_ant_trial():
     optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-    study_name = "cheetah_trial_short_max_length"  # Unique identifier of the study.
+    study_name = "ant_trial_1"  # Unique identifier of the study.
     storage_name = "sqlite:///{}.db".format(study_name)
     study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True, direction='maximize')
 
-    study.optimize(objective_cheetah, n_trials=10, n_jobs=1)
+    study.optimize(objective_ant, n_trials=20, n_jobs=1)
 
     print(f"Best params: {study.best_params}")
     print(f"Best value: {study.best_value}")
 
 if __name__ == "__main__":
-    perform_cheetah_trial()
+    perform_ant_trial()
