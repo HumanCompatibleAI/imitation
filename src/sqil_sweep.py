7815345eda13adeee885050aea7e230854901e14
lukasberglund <lukastberglund@gmail.com>
2023-09-15 19:56:34 +0200

    save

--- src/sqil_sweep.py
+++ src/sqil_sweep.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import logging
 import os
@@ -10,7 +11,18 @@ from imitation.util import networks, util
 from run_train_imitation import LOGDIR
 
 from stable_baselines3.sac import policies as SACPolicies
-
+fast_configs = [
+        "environment.fast",
+        "demonstrations.fast",
+        "policy_evaluation.fast",
+        "fast"]
+environments = [
+    ("cheetah", "seals/HalfCheetah-v1"),
+    ("ant", "seals/Ant-v1"),
+    ("walker", "seals/Walker2d-v1"),
+    ("hopper", "seals/Hopper-v1"),
+    ("swimmer", "seals/Swimmer-v1"),
+]
 
 def get_sqil_results(config, named_configs=[]):
     config_str = json.dumps(config)
@@ -27,90 +39,21 @@ def get_sqil_results(config, named_configs=[]):
     print(results)
     return results["imit_stats"]["return_mean"]
 
-def objective_cartpole(trial, environment):
-    # TODO: Add more hyperparameters
-    learning_rate = trial.suggest_loguniform('learning_rate', 3e-6, 1e-1)
-    batch_size = trial.suggest_categorical('batch_size', [16, 32, 64])
-
-    config = {"rl.rl_kwargs": {
-            "learning_rate": learning_rate,
-            "batch_size": batch_size,
-        }}
-    
-    return get_sqil_results(config, environment)
-
-    # Convert config to CLI arguments or a temporary file
-
-def objective_cheetah(trial):
-
+def objective(trial, environment):
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True) # too broad?
-    batch_size = trial.suggest_int("batch_size", 10, 10_000) # might be too broad
+    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64]) 
     gradient_steps = trial.suggest_int("gradient_steps", 1, 20)
     tau = trial.suggest_float("tau", 0.001, 0.5, log=True)
     gamma = trial.suggest_float("gamma", 0.5, 0.999)
-    total_timesteps = trial.suggest_int("total_timesteps", 10_000, 100_000, log=True)
 
     config_updates = {
+        "environment.gym_id": environment,
         "demonstrations.n_expert_demos": 50,
-        "sqil.total_timesteps": total_timesteps, # changed for speed
-        # "policy.policy_cls": SACPolicies.SACPolicy,
+        "sqil.total_timesteps": 100_000, 
 
         "sqil.train_kwargs": {
             "progress_bar": True,
-        },
-        "rl.rl_kwargs": {
-            "learning_rate": learning_rate,
-            "batch_size": batch_size,
-            "gradient_steps": gradient_steps,
-            "tau": tau,
-            "gamma": gamma,
-            "learning_starts": 1000,
-        },
-        # "expert.loader_kwargs": {
-        #     # "organization": "sb3",
-        #     # "algo_name": "ppo",
-        # },
-        # "environment": {
-        #     "gym_id": "seals/HalfCheetah-v0"
-        # },
-    }
-
-    named_configs = ["rl.sac", "policy.sac256", "seals_half_cheetah"]
-
-
-    return get_sqil_results(config_updates, named_configs)    
-
-def perform_cheetah_trial():
-    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-    study_name = "cheetah_trial_short_max_length2"  # Unique identifier of the study.
-    storage_name = "sqlite:///{}.db".format(study_name)
-    study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True, direction='maximize')
-
-    study.optimize(objective_cheetah, n_trials=20, n_jobs=1)
-
-    print(f"Best params: {study.best_params}")
-    print(f"Best value: {study.best_value}")
-
-
-def objective_ant(trial):
-
-    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True) # too broad?
-    batch_size = trial.suggest_int("batch_size", 10, 10_000) # might be too broad
-    gradient_steps = trial.suggest_int("gradient_steps", 1, 20)
-    tau = trial.suggest_float("tau", 0.001, 0.5, log=True)
-    gamma = trial.suggest_float("gamma", 0.5, 0.999)
-    total_timesteps = trial.suggest_int("total_timesteps", 10_000, 200_000, log=True)
-    # if things don't work maybe changing entropy coeff will help
-
-    config_updates = {
-        "environment.gym_id": "seals/Ant-v1",
-
-        "demonstrations.n_expert_demos": 50,
-        "sqil.total_timesteps": total_timesteps, 
-        # "policy.policy_cls": SACPolicies.SACPolicy,
-
-        "sqil.train_kwargs": {
-            "progress_bar": True,
+            "log_interval": 100,
         },
         "rl.rl_kwargs": {
             "learning_rate": learning_rate,
@@ -125,22 +68,24 @@ def objective_ant(trial):
         },
     }
 
-    named_configs = ["rl.sac", "policy.sac256"]
-
+    named_configs = ["rl.sac", "policy.sac256"] + fast_configs # DELETE FAST CONFIGS
 
     return get_sqil_results(config_updates, named_configs)    
 
-
-def perform_ant_trial():
+def perform_trial(study_name, environment, n_trials, n_jobs):
     optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-    study_name = "ant_trial_1"  # Unique identifier of the study.
     storage_name = "sqlite:///{}.db".format(study_name)
     study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True, direction='maximize')
 
-    study.optimize(objective_ant, n_trials=20, n_jobs=1)
+    trial_objective = functools.partial(objective, environment=environment)
+
+    study.optimize(trial_objective, n_trials=n_trials, n_jobs=n_jobs)
 
     print(f"Best params: {study.best_params}")
     print(f"Best value: {study.best_value}")
 
 if __name__ == "__main__":
-    perform_ant_trial()
+    n_trials, n_jobs = 40, 10
+    n_trials, n_jobs = 1, 1 # DELETE THIS
+    for name, env in environments:
+        perform_trial(name, env)
