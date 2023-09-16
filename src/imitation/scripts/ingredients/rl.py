e55ab9d5c2608cd27e98d7ad7376c694ee75334f
Adam Gleave <adam@gleave.me>
2023-09-15 17:33:00 -0700

    Simplify RL hook

--- src/imitation/scripts/ingredients/rl.py
+++ src/imitation/scripts/ingredients/rl.py
@@ -40,13 +40,14 @@ def config():
 
 @rl_ingredient.config_hook
 def config_hook(config, command_name, logger):
-    """Sets defaults equivalent to sb3.PPO default hyperparameters."""
+    """Sets defaults equivalent to sb3.PPO default hyperparameters.
+
+    This hook is a no-op if command_name is "sqil" (used only in train_imitation),
+    which has its own config hook.
+    """
     del logger
     res = {}
-    uses_default_rl_cls_or_ppo = (
-        config["rl"]["rl_cls"] is None or config["rl"]["rl_cls"] == sb3.PPO
-    )
-    if uses_default_rl_cls_or_ppo and command_name != "sqil":
+    if config["rl"]["rl_cls"] == None and command_name != "sqil":
         res["rl_cls"] = sb3.PPO
         res["batch_size"] = 2048  # rl_kwargs["n_steps"] = batch_size // venv.num_envs
         res["rl_kwargs"] = dict(
