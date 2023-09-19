2cfb17a5b06ea0d85311ef6c7419b8644e290bce
Maximilian Ernestus <maximilian@ernestus.de>
2023-09-19 11:13:44 +0200

    Set seals version to upstream again.

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ IS_NOT_WINDOWS = os.name != "nt"
 
 PARALLEL_REQUIRE = ["ray[debug,tune]~=2.0.0"]
 ATARI_REQUIRE = [
-    # TODO: revert when https://github.com/HumanCompatibleAI/seals/pull/77 is
-    #   released
-    "seals[atari] @ git+https://github.com/HumanCompatibleAI/seals.git@typing-fixes-py39",
+    "seals[atari]~=0.2.1",
 ]
 PYTYPE = ["pytype==2022.7.26"] if IS_NOT_WINDOWS else []
 
@@ -196,9 +194,7 @@ setup(
         "tqdm",
         "rich",
         "scikit-learn>=0.21.2",
-        # TODO: revert when https://github.com/HumanCompatibleAI/seals/pull/77 is
-        #   released
-        "seals @ git+https://github.com/HumanCompatibleAI/seals.git@typing-fixes-py39",
+        "seals~=0.2.1",
         "stable-baselines3~=2.0",
         "sacred>=0.8.4",
         "tensorboard>=1.14",
