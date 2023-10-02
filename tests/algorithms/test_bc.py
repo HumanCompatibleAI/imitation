6aa25ff3091a065c64838945bceaab8aaad918cb
ZiyueWang25 <wfuymu@gmail.com>
2023-10-02 11:24:43 -0700

    remove FloatReward

--- tests/algorithms/test_bc.py
+++ tests/algorithms/test_bc.py
@@ -291,18 +291,9 @@ def test_that_policy_reconstruction_preserves_parameters(
         th.testing.assert_close(original, reconstructed)
 
 
-# TODO: remove after https://github.com/DLR-RM/stable-baselines3/pull/1676 merged
-class FloatReward(gym.RewardWrapper):
-    """Typecasts reward to a float."""
-
-    def reward(self, reward):
-        return float(reward)
-
-
 def test_dict_space():
     def make_env():
         env = sb_envs.SimpleMultiObsEnv(channel_last=False)
-        env = FloatReward(env)
         return RolloutInfoWrapper(env)
 
     env = vec_env.DummyVecEnv([make_env, make_env])
