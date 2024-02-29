a2856393adcce2831e60b1f1d1550e94db7e6a19
Darryl Wright <darrylwright@Darryls-MacBook-Pro.local>
2024-02-29 15:20:39 -0600

    Remove querent_kwargs from super().__init__

--- src/imitation/algorithms/preference_comparisons.py
+++ src/imitation/algorithms/preference_comparisons.py
@@ -1453,7 +1453,7 @@ class ZooniverseGatherer(PrefCollectGatherer):
             rng: random number generator, if applicable.
             custom_logger: Where to log to; if None (default), creates a new logger.
         """
-        super().__init__(pref_collect_address, wait_for_user, rng, custom_logger, querent_kwargs)
+        super().__init__(pref_collect_address, wait_for_user, rng, custom_logger)
         self.querent = ZooniverseQuerent(
             **querent_kwargs,
         )
